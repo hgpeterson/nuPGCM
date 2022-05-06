6d167cb9fdd3e39102840c3867280e8985340547
Henry Peterson <hgpeterson@caltech.edu>
2022-05-05 17:05:59 -0700

    -added types -using args... -gradients don't need m

--- nuPGCM/src/ThreeDimensionalModel/operators.jl
+++ nuPGCM/src/ThreeDimensionalModel/operators.jl
@@ -2,37 +2,40 @@
 #     return evaluate(u, m.p₀, m.p, m.t, m.C₀)
 # end
 
-function ∂ξ(m, u, p₀)
+function ∂ᵢ(u::AbstractArray{<:Real,1}, p₀::AbstractArray{<:Real,1}, p::AbstractArray{<:Real,2}, 
+            t::AbstractArray{<:Real,2}, C₀::AbstractArray{<:Real,3}; i::Integer)
     # find triangle p₀ is in
-    k = get_tri(p₀, m.p, m.t)
+    k = get_tri(p₀, p, t)
 
     # evaluate there
-    return ∂ξ(m, u, p₀, k)
+    return ∂ᵢ(u, p₀, k, p, t, C₀; i)
 end
-function ∂ξ(m, u, p₀, k)
+function ∂ᵢ(u::AbstractArray{<:Real,1}, p₀::AbstractArray{<:Real,1}, k::Integer, p::AbstractArray{<:Real,2}, 
+            t::AbstractArray{<:Real,2}, C₀::AbstractArray{<:Real,3}; i::Integer)
     # sum weighted combinations of c₂
-    return dot(u[m.t[k, :]], m.C₀[k, 2, :])
+    return dot(u[t[k, :]], C₀[k, i+1, :])
+end
+function ∂ᵢ(m::ModelSetup3DPG, u::AbstractArray{<:Real,1}, p₀::AbstractArray{<:Real,1}; i::Integer)
+    return ∂ᵢ(u, p₀, m.p, m.t, m.C₀; i)
+end
+function ∂ᵢ(m::ModelSetup3DPG, u::AbstractArray{<:Real,1}, p₀::AbstractArray{<:Real,1}, k::Integer; i::Integer)
+    return ∂ᵢ(u, p₀, k, m.p, m.t, m.C₀; i)
 end
 
-function ∂η(m, u, p₀)
-    # find triangle p₀ is in
-    k = get_tri(p₀, m.p, m.t)
-
-    # evaluate there
-    return ∂η(m, u, p₀, k)
+function ∂ξ(args...)
+    return ∂ᵢ(args...; i=1)
 end
-function ∂η(m, u, p₀, k)
-    # sum weighted combinations of c₃
-    return dot(u[m.t[k, :]], m.C₀[k, 3, :])
+function ∂η(args...)
+    return ∂ᵢ(args...; i=2)
 end
 
-function curl(m::ModelSetup3DPG, u::AbstractArray{<:Real,2}, p₀)
+function curl(m::ModelSetup3DPG, u::AbstractArray{<:Real,2}, p₀::AbstractArray{<:Real,1})
     # find triangle p₀ is in
     k = get_tri(p₀, m.p, m.t)
 
     # evaluate there
     return curl(m, u, p₀, k)
 end
-function curl(m::ModelSetup3DPG, u::AbstractArray{<:Real,2}, p₀, k)
+function curl(m::ModelSetup3DPG, u::AbstractArray{<:Real,2}, p₀::AbstractArray{<:Real,1}, k::Integer)
     return ∂ξ(m, u[2, :], p₀, k) - ∂η(m, u[1, :], p₀, k)
 end
