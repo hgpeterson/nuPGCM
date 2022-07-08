4608d831a518134291bca0abcce43744dde236d2
Henry Peterson <hgpeterson@caltech.edu>
2022-07-07 17:00:51 -0700

    add 2D results

--- examples/1Demulator.jl
+++ examples/1Demulator.jl
@@ -1,5 +1,6 @@
 using nuPGCM
 using PyPlot
+using Printf
 
 plt.style.use("../plots.mplstyle")
 plt.close("all")
@@ -7,13 +8,13 @@ pygui(false)
 
 set_out_folder("../output/")
 
-function single_column_inversion(; bl=false)
+function emulate_1D(; bl=false)
     # parameters (see `setup.jl`)
     f = 8.753044701640954e-5 
     N2 = 1e-6
     nz = 2^8
-    H = 1613.8975181510887
-    θ = 0.0024260899218681575
+    H = fem_evaluate(m3D, m3D.H, ξ₀, η₀)
+    θ = -atan(fem_evaluate(m3D, m3D.Hx, ξ₀, η₀))
     transport_constraint = true
     U = [0.0]
 
@@ -47,28 +48,39 @@ function single_column_inversion(; bl=false)
     return m, s
 end
 
-m1D, s1D = single_column_inversion()
-
-# get 3D u, v
-ξ₀ = 4e6
+# comparison point
+# ξ₀ = 4e6
+# ξ₀ = m2D.ξ[206]
+ξ₀ = m2D.ξ[50]
 η₀ = 0
-uξ = zeros(m.nσ)
-uη = zeros(m.nσ)
-for j=1:m.nσ
-    uξ[j] = nuPGCM.fem_evaluate(m, s.uξ[:, j], ξ₀, η₀)
-    uη[j] = nuPGCM.fem_evaluate(m, s.uη[:, j], ξ₀, η₀)
+m1D, s1D = emulate_1D()
+
+# get 2D ux, uy
+iξ = argmin(abs.(m2D.ξ .- ξ₀))
+uξ2D = s2D.uξ[iξ, :]
+uη2D = s2D.uη[iξ, :]
+
+# get 3D ux, uy
+uξ3D = zeros(m3D.nσ)
+uη3D = zeros(m3D.nσ)
+for j=1:m3D.nσ
+    uξ3D[j] = fem_evaluate(m3D, s3D.uξ[:, j], ξ₀, η₀)
+    uη3D[j] = fem_evaluate(m3D, s3D.uη[:, j], ξ₀, η₀)
 end
 
 # plot u
 fig, ax = subplots(1, 2, figsize=(2*1.955, 3.176), sharey=true)
-ax[1].set_xlabel(L"Zonal velocity $u^\xi$ (10$^{-3}$ m s$^{-1}$)")
-ax[2].set_xlabel(L"Meridional velocity $u^\eta$ (10$^{-3}$ m s$^{-1}$)")
+ax[1].set_title(latexstring(L"Comparison point: $x = $", @sprintf("%d", ξ₀/1e3), " km")) 
+ax[1].set_xlabel(L"Zonal velocity $u^x$ ($\times$ 10$^{-3}$ m s$^{-1}$)")
+ax[2].set_xlabel(L"Meridional velocity $u^y$ ($\times$ 10$^{-3}$ m s$^{-1}$)")
 ax[1].set_ylabel(L"Vertical coordinate $z$ (km)")
 ax[1].plot(1e3*s1D.u, m1D.z/1e3, label="1D")
-ax[1].plot(1e3*uξ, m1D.z/1e3, label="3D")
+ax[1].plot(1e3*uξ2D, m1D.z/1e3, label="2D")
+ax[1].plot(1e3*uξ3D, m1D.z/1e3, label="3D", c="k", ls="--", lw=0.5)
 ax[2].plot(1e3*s1D.v, m1D.z/1e3, label="1D")
-ax[2].plot(1e3*uη, m1D.z/1e3, label="3D")
+ax[2].plot(1e3*uη2D, m1D.z/1e3, label="2D")
+ax[2].plot(1e3*uη3D, m1D.z/1e3, label="3D", c="k", ls="--", lw=0.5)
 ax[1].legend()
-savefig("images/uv_column.png")
-println("images/uv_column.png")
+savefig("images/ux_uy_column.png")
+println("images/ux_uy_column.png")
 plt.close()
