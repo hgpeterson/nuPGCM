a7ac09ec39172b1192f4a37a7a3293e3ad762e21
Henry Peterson <hgpeterson@caltech.edu>
2022-09-12 15:55:23 -0700

    cleanup

--- meshes/bowl.jl
+++ meshes/bowl.jl
@@ -13,7 +13,7 @@ pygui(false)
 Find all boundary nodes `e` in the triangulation `t`.
 """
 function boundary_nodes(t)
-    edges, boundary_indices = nuPGCM.all_edges(t)
+    edges, boundary_indices = all_edges(t)
     return unique(edges[boundary_indices,:][:])
 end
 
@@ -33,59 +33,21 @@ function generate_bowl_mesh(h₀, r)
     # model
     gmsh.model.add("bowl_mesh")
 
-    # # quick square
-    # gmsh.model.geo.addPoint(-1, 0, 0, h₀)
-    # gmsh.model.geo.addPoint(-1, -1, 0, h₀)
-    # gmsh.model.geo.addPoint(1, -1, 0, h₀)
-    # gmsh.model.geo.addPoint(1, 0, 0, h₀)
-    # N = 4
-
     # depth function
     z(x) = x^2 - 1
-    # ∂ₓz(x) = 2*x
-    # z(x) = -1
     
     # edge points
-    x = -1:h₀/r:1
-    n = size(x, 1)
-    for i=1:n
+    N = Int64(round(2/(h₀/r)))/2
+    x = -1:2/(N - 1):1
+    for i=1:N
         gmsh.model.geo.addPoint(x[i], z(x[i]), 0, h₀)
     end
-    for i=(n-1):-1:2
-        gmsh.model.geo.addPoint(x[i], 0, 0, h₀)
-    end
-    N = 2*n - 2
 
-    # # edge points
-    # x = -1
-    # N = 0
-    # while z(x) <= 0
-    #     if x > 0 && z(x) > -h₀/r/2
-    #         break
-    #     end
-    #     gmsh.model.geo.addPoint(x, z(x), 0, h₀)
-    #     x += h₀/r/sqrt(1 + ∂ₓz(x)^2)
-    #     N += 1
-    # end
-    # gmsh.model.geo.addPoint(1, 0, 0, h₀)
-    # N += 1
-    
     # connect edge points by lines
     for i=1:N-1
         gmsh.model.geo.addLine(i, i + 1)
     end
     gmsh.model.geo.addLine(N, 1)
-
-    # for x=-1:0.5:1
-    #     gmsh.model.geo.addPoint(x, z(x), 0, h₀)
-    # end
-    # gmsh.model.geo.addPoint(1, 0, 0, h₀)
-    # gmsh.model.geo.addPoint(-1, 0, 0, h₀)
-    # gmsh.model.geo.addSpline(1:5, 1)
-    # gmsh.model.geo.addLine(5, 6, 2)
-    # gmsh.model.geo.addLine(6, 7, 3)
-    # gmsh.model.geo.addLine(7, 1, 4)
-    # N = 4
     
     # loop curves together and define surface
     gmsh.model.geo.addCurveLoop(1:N, 1)
@@ -136,7 +98,7 @@ where `p` is an n_nodes × n_dims array of node positions, `t` is an
 n_tri × 3 array of triangle node indices, and `e` is an n_edge_nodes × 1 array of
 edge node indices.
 """
-function load_gmesh(; h5save=false)
+function load_gmesh(; savefile="")
     # initialize mesh and load from file
     gmsh.initialize()
     gmsh.option.setNumber("General.Terminal", 1)
@@ -165,23 +127,27 @@ function load_gmesh(; h5save=false)
     gmsh.finalize()
 
     # save as h5
-    if h5save
-        file = h5open("mesh.h5", "w")
+    if savefile != ""
+        file = h5open(savefile, "w")
         write(file, "p", p)
         write(file, "t", t)
         write(file, "e", e)
         close(file)
-        println("mesh.h5")
+        println(savefile)
     end
 
     return p, t, e
 end
 
-generate_bowl_mesh(0.02, 1)
-p, t, e = load_gmesh(h5save=true)
-tplot(p, t)
-plot(p[e, 1], p[e, 2], "o", ms=1)
-plt.axis("equal")
-savefig("mesh.png")
-println("mesh.png")
-plt.close()
+for i=0:5
+    h₀ = 0.01*2^(5-i)
+    generate_bowl_mesh(h₀, 1)
+    p, t, e = load_gmesh(savefile="gmsh/mesh$i.h5")
+
+    tplot(p, t)
+    plot(p[e, 1], p[e, 2], "o", ms=1)
+    plt.axis("equal")
+    savefig("gmsh/mesh$i.png")
+    println("gmsh/mesh$i.png")
+    plt.close()
+end
