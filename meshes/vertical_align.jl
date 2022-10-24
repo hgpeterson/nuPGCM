bb256ea335ccc145a1904b140e251d5029b5a5b9
Henry Peterson <hgpeterson@caltech.edu>
2022-10-24 11:23:04 -0700

    vertically aligned mesh

---
+++ meshes/vertical_align.jl
@@ -0,0 +1,59 @@
+using nuPGCM
+using Delaunay
+using HDF5
+using PyPlot
+
+plt.style.use("../plots.mplstyle")
+plt.close("all")
+pygui(false)
+
+function vertical_align(h)
+    x = -1:h:1
+    nx = size(x, 1)
+
+    H = @. sqrt(2 - x^2) - 1
+
+    p = [-1.0      0.0
+         -1.0+h    0.0
+         -1.0+h   -H[2]]
+    e = [1, 2, 3]
+    for i=3:nx-2
+        n = size(p, 1)
+        nz = Int64(ceil(H[i]/h))
+        z = range(-H[i], 0, length=nz)
+        p = vcat(p, [x[i]*ones(nz) z])
+        push!(e, n+1)
+        push!(e, size(p, 1))
+    end
+    n = size(p, 1)
+    p = vcat(p, [1.0-h    0.0
+                 1.0-h   -H[end-1]
+                 1.0      0.0])
+    push!(e, n+1, n+2, n+3)
+
+    mesh = delaunay(p)
+    t = mesh.simplices
+
+    println("np = ", size(p, 1))
+    if size(p, 1) < 1000
+        fig, ax, im = tplot(p, t)
+        # ax.plot(p[:, 1], p[:, 2], "o", ms=1)
+        # ax.plot(p[e, 1], p[e, 2], "o", ms=1)
+        ax.axis("equal")
+        savefig("mesh.png")
+        println("mesh.png")
+        plt.close()
+    end
+
+    file = h5open("mesh0.h5", "w")
+    write(file, "p", p)
+    write(file, "t", t)
+    write(file, "e", e)
+    close(file)
+    println("mesh0.h5")
+
+    return p, t, e
+end
+
+p, t, e = vertical_align(0.04)
+println("Done.")
