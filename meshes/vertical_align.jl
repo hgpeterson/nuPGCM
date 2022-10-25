aba1f9fe82ce495c0863186d5d30453bc8c7198f
Henry Peterson <hgpeterson@caltech.edu>
2022-10-25 16:03:09 -0700

    different h for x and z

--- meshes/vertical_align.jl
+++ meshes/vertical_align.jl
@@ -7,19 +7,19 @@ plt.style.use("../plots.mplstyle")
 plt.close("all")
 pygui(false)
 
-function vertical_align(h)
-    nx = Int64(ceil(2/h))
+function vertical_align(hx, hz)
+    nx = Int64(ceil(2/hx))
     x = range(-1, 1, length=nx)
 
     H = @. sqrt(2 - x^2) - 1
 
-    p = [-1.0      0.0
-         -1.0+h    0.0
-         -1.0+h   -H[2]]
+    p = [-1.0       0.0
+         -1.0+hx    0.0
+         -1.0+hx   -H[2]]
     e = [1, 2, 3]
     for i=3:nx-2
         n = size(p, 1)
-        nz = Int64(ceil(H[i]/h))
+        nz = Int64(ceil(H[i]/hz))
         if nz == 2
             nz += 1
         end
@@ -27,19 +27,14 @@ function vertical_align(h)
         #     nz += 1
         # end
         z = range(-H[i], 0, length=nz)
-        noise = h/4*rand(nz)
-        noise[[1, end]] .= 0
-        # z += noise
         p = vcat(p, [x[i]*ones(nz) z])
-        # p = vcat(p, [(x[i] .+ noise) z])
-        # p = vcat(p, [(x[i] .+ noise) (z + noise)])
         push!(e, n+1)
         push!(e, size(p, 1))
     end
     n = size(p, 1)
-    p = vcat(p, [1.0-h    0.0
-                 1.0-h   -H[end-1]
-                 1.0      0.0])
+    p = vcat(p, [1.0-hx    0.0
+                 1.0-hx   -H[end-1]
+                 1.0       0.0])
     push!(e, n+1, n+2, n+3)
 
     # p = [-1.0  0.0
@@ -55,8 +50,8 @@ function vertical_align(h)
     println("np = ", size(p, 1))
     if size(p, 1) < 1000
         fig, ax, im = tplot(p, t)
-        ax.plot(p[:, 1], p[:, 2], "o", ms=1)
-        ax.plot(p[e, 1], p[e, 2], "o", ms=1)
+        # ax.plot(p[:, 1], p[:, 2], "o", ms=1)
+        # ax.plot(p[e, 1], p[e, 2], "o", ms=1)
         ax.axis("equal")
         savefig("mesh.png")
         println("mesh.png")
@@ -73,17 +68,5 @@ function vertical_align(h)
     return p, t, e
 end
 
-# p, t, e = vertical_align(0.01)
-# println("Done.")
-
-file = h5open("jc/mesh1.h5", "r")
-p = read(file, "p")
-t = read(file, "t")
-close(file)
-fig, ax, im = tplot(p, t)
-ax.axis("equal")
-savefig("mesh.png")
-println("mesh.png")
-plt.close()
-
-println("mesh0.h5")
+p, t, e = vertical_align(0.01, 0.01)
+println("Done.")
