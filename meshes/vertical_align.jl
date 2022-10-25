b5e7b4e1a62ad1dc1a67db14c7754fda164564f1
Henry Peterson <hgpeterson@caltech.edu>
2022-10-24 20:57:48 -0700

    random noise

--- meshes/vertical_align.jl
+++ meshes/vertical_align.jl
@@ -8,8 +8,8 @@ plt.close("all")
 pygui(false)
 
 function vertical_align(h)
-    x = -1:h:1
-    nx = size(x, 1)
+    nx = Int64(ceil(2/h))
+    x = range(-1, 1, length=nx)
 
     H = @. sqrt(2 - x^2) - 1
 
@@ -20,8 +20,19 @@ function vertical_align(h)
     for i=3:nx-2
         n = size(p, 1)
         nz = Int64(ceil(H[i]/h))
+        if nz == 2
+            nz += 1
+        end
+        # if rand() > 0.5
+        #     nz += 1
+        # end
         z = range(-H[i], 0, length=nz)
+        noise = h/4*rand(nz)
+        noise[[1, end]] .= 0
+        # z += noise
         p = vcat(p, [x[i]*ones(nz) z])
+        # p = vcat(p, [(x[i] .+ noise) z])
+        # p = vcat(p, [(x[i] .+ noise) (z + noise)])
         push!(e, n+1)
         push!(e, size(p, 1))
     end
@@ -31,14 +42,21 @@ function vertical_align(h)
                  1.0      0.0])
     push!(e, n+1, n+2, n+3)
 
+    # p = [-1.0  0.0
+    #       0.0  0.0
+    #       0.0 -0.5
+    #       0.0 -1.0
+    #       1.0  0.0]
+    # e = [1, 2, 4, 5]
+
     mesh = delaunay(p)
     t = mesh.simplices
 
     println("np = ", size(p, 1))
     if size(p, 1) < 1000
         fig, ax, im = tplot(p, t)
-        # ax.plot(p[:, 1], p[:, 2], "o", ms=1)
-        # ax.plot(p[e, 1], p[e, 2], "o", ms=1)
+        ax.plot(p[:, 1], p[:, 2], "o", ms=1)
+        ax.plot(p[e, 1], p[e, 2], "o", ms=1)
         ax.axis("equal")
         savefig("mesh.png")
         println("mesh.png")
@@ -55,5 +73,17 @@ function vertical_align(h)
     return p, t, e
 end
 
-p, t, e = vertical_align(0.04)
-println("Done.")
+# p, t, e = vertical_align(0.01)
+# println("Done.")
+
+file = h5open("jc/mesh1.h5", "r")
+p = read(file, "p")
+t = read(file, "t")
+close(file)
+fig, ax, im = tplot(p, t)
+ax.axis("equal")
+savefig("mesh.png")
+println("mesh.png")
+plt.close()
+
+println("mesh0.h5")
