3f63e745bf42b744f567380a0f4f9ff6bc902606
Henry Peterson <hgpeterson@caltech.edu>
2023-01-17 17:05:29 -0800

    cleanup after success

--- meshes/valign3D.jl
+++ meshes/valign3D.jl
@@ -22,10 +22,6 @@ function valign3D(ifile; savefile=nothing)
     nt_circ = size(t_circ, 1)
     ne_circ = size(e_circ, 1)
 
-    # normals = [sign(cross(vcat(p_circ[t_circ[k, 1], :] - p_circ[t_circ[k, 2], :], 0),
-    #                       vcat(p_circ[t_circ[k, 1], :] - p_circ[t_circ[k, 3], :], 0))[3]) for k ∈ axes(t_circ, 1)]
-    # println(all(normals .== 1)) # returns true → all triangles have counterclockwise orientation
-
     # interior
     interior = findall(!in(e_circ), 1:np_circ)
 
@@ -83,63 +79,6 @@ function valign3D(ifile; savefile=nothing)
     # compute tessellation
     t = [0 0 0 0] # allocate
     for k=1:nt_circ
-        # # local delaunay tessellation of column
-        # i_col = vcat(tri_to_p[k, 1][:], tri_to_p[k, 2][:], tri_to_p[k, 3][:])
-        # t_col = delaunay(p[i_col, :]).simplices
-
-        # # remove zeros vols
-        # t_col_g = i_col[t_col]
-        # vols = [abs(det([p[t_col_g[k, j+1], i] - p[t_col_g[k, 1], i] for i=1:3, j=1:3])) for k ∈ axes(t_col_g, 1)]
-        # keep = vols .!= 0
-        # if !all(keep)
-        #     # save vtu of bad egg
-        #     cells = [MeshCell(VTKCellTypes.VTK_TETRA, t_col_g[i, :]) for i ∈ axes(t_col_g, 1)]
-        #     vtk_grid("col$k.vtu", p', cells) do vtk
-        #         bad_egg = zeros(size(p, 1))
-        #         bad_egg[t_col_g[.!keep, :]] .= 1
-        #         vtk["bad egg"] = bad_egg
-        #     end
-        #     println("col$k.vtu")
-
-        #     # just tessellate first prism?
-        #     i_col_12 = vcat(tri_to_p[k, 1][1:2], tri_to_p[k, 2][1:2], tri_to_p[k, 3][1:2])
-        #     t_col_12 = delaunay(p[i_col_12, :]).simplices
-        #     t_col_12g = i_col_12[t_col_12]
-        #     cells = [MeshCell(VTKCellTypes.VTK_TETRA, t_col_12g[i, :]) for i ∈ axes(t_col_12g, 1)]
-        #     vtk_grid("col$(k)_top.vtu", p', cells) do vtk
-        #     end
-        #     println("col$(k)_top.vtu")
-
-        #     # mesh faces
-        #     lens = length.(tri_to_p[k, :])
-        #     tri_edges = [(1,2), (2,3), (3,1)]
-        #     faces = []
-        #     ts = []
-        #     for (i, (e1, e2)) in enumerate(tri_edges)
-        #         face = vcat(p[tri_to_p[k, e1][:], :], p[tri_to_p[k, e2][:], :])
-        #         l = norm(face[1, :] - face[lens[i]+1, :])
-        #         face[1:lens[i], 1] .= isempty(faces) ? 0 : faces[end][end, 1]
-        #         face[lens[i]+1:end, 1] .= l + face[1, 1]
-        #         face = face[:, [1, 3]]
-        #         push!(faces, face)
-        #         push!(ts, delaunay(face).simplices)
-        #     end
-
-        #     # plot
-        #     fig, ax = subplots(1)
-        #     for i=1:3
-        #         tplot(faces[i], ts[i], fig=fig, ax=ax)
-        #         ax.plot(faces[i][:, 1], faces[i][:, 2], "o", ms=1)
-        #     end
-        #     ax.set_xlim(-0.1, faces[3][end, 1]+0.1)
-        #     ax.set_ylim(-1.1, 0.1)
-        #     savefig("faces$k.png")
-        #     plt.close()
-        # end
-        # t_col = t_col[keep, :]
-
-        # # add to global t
-        # t = [t; i_col[t_col]]
         # column lengths
         lens = length.(tri_to_p[k, :])
 
@@ -175,28 +114,6 @@ function valign3D(ifile; savefile=nothing)
 
     return p, t, e
 end
-
-# p, t, e = valign3D("circle/mesh2.h5"; savefile="mesh.h5")
-
-# fmap, faces, bndix = all_faces(t)
-# cells = [MeshCell(VTKCellTypes.VTK_TRIANGLE, faces[i, :]) for i in bndix]
-# vtk_grid("faces.vtu", p', cells) do vtk
-# end
-# println("faces.vtu")
-
-# cells = [MeshCell(VTKCellTypes.VTK_TETRA, t[i, :]) for i in axes(t, 1)]
-# vtk_grid("mesh1.vtu", p', cells) do vtk
-# end
-# println("mesh1.vtu")
-
-# p, t, e = nuPGCM.add_nodes(p, t, e, 2)
-# cells = [MeshCell(VTKCellTypes.VTK_QUADRATIC_TETRA, t[i, :]) for i in axes(t, 1)]
-# vtk_grid("mesh2.vtu", p', cells) do vtk
-#     bdy = zeros(size(p, 1))
-#     bdy[e] .= 1
-#     vtk["boundary"] = bdy
-# end
-# println("mesh2.vtu")
 
 for i=0:2
     valign3D("circle/mesh$i.h5"; savefile="valign3D/mesh$i.h5")
