5fb810a9c729ad07da07bb332070b283580d8a60
Henry Peterson <hgpeterson@caltech.edu>
2023-02-08 16:49:04 -0800

    reorganize grid to DG

--- scratch/toyDG1D.jl
+++ scratch/toyDG1D.jl
@@ -1,4 +1,5 @@
 using nuPGCM
+using WriteVTK
 using PyPlot
 using SparseArrays
 using LinearAlgebra
@@ -9,24 +10,58 @@ pygui(false)
 
 
 """
-    -∂zz(u) + u = ∂x(f),
-BC:
-    • u = 0 at z = 0
-    • u = 0 at z = -H
-    # • ∫ zu dz = 0
+Solve
+    -∂zz(u) + u = 0,
+with
+    • u = 0 at z = 0,
+    • u = 0 at z = -H.
 """
 function solve_toyDG1D(g)
-    cols = get_cols(g.p, g.t)
-    # N = sum(length(unique(g.t[cols[i], :][:])) for i ∈ eachindex(cols))
-    N = 2*g.np - 2
+    # for element matricies
+    s = ShapeFunctionIntegrals(g.s, g.s)
+    J = Jacobians(g)
 
+    # separate mesh into columns
+    cols = get_cols(g.p, g.t)
+
+    # get p, t, e for dg mesh
+    p_dg, t_dg, e_dg = get_pte_dg(g.p, g.t, g.e, cols)
+    N = size(p_dg, 1) # should be 2*g.np - 2
+
+    # stamp
     A = Tuple{Int64,Int64,Float64}[]
     r = zeros(N)
-    for i ∈ eachindex(cols), k ∈ cols[i]
+    for col ∈ cols, k ∈ col
+        JJ = J.Js[k, :, end]*J.Js[k, :, end]'
+        K = J.dets[k]*sum(s.K.*JJ, dims=(1, 2))[1, 1, :, :]
+        M = J.dets[k]*s.M
+        for i=1:g.nn, j=1:g.nn
+            if t_dg[k, i] ∉ e_dg
+                push!(A, (t_dg[k, i], t_dg[k, j], K[i, j]))
+                push!(A, (t_dg[k, i], t_dg[k, j], M[i, j]))
+            end
+        end
+        # r[t_dg[k, :]] = M*ones(g.nn)
     end
-end
 
-function toyDG1D(; order)
+    # dirichlet
+    for i ∈ e_dg
+        push!(A, (i, i, 1))
+        r[i] = 1
+    end
+
+    # sparse matrix
+    A = sparse((x->x[1]).(A), (x->x[2]).(A), (x->x[3]).(A), N, N)
+
+    # solve
+    u = A\r
+
+    # save as .vtu
+    points = p_dg'
+    cells = [MeshCell(VTKCellTypes.VTK_TRIANGLE, t_dg[i, :]) for i ∈ axes(t_dg, 1)]
+    vtk_grid("output/u.vtu", points, cells) do vtk
+        vtk["u"] = u
+    end
 end
 
 function get_cols(p, t)
@@ -42,6 +77,7 @@ function get_cols(p, t)
     cols = [Int64[] for i=1:ncols]
 
     for k ∈ axes(t, 1)
+        # find which column centroid in x lives in
         x̄ = sum(p[t[k, :], 1])/3
         i = searchsorted(x, x̄).stop
         push!(cols[i], k)
@@ -50,37 +86,67 @@ function get_cols(p, t)
     return cols
 end
 
-# p = [ 0.0   0.0
-#       0.1   0.0
-#       0.0  -0.25
-#       0.1  -0.25
-#       0.0  -0.5
-#       0.1  -0.5
-#       0.0  -0.75
-#       0.1  -0.75
-#       0.0  -1.0
-#       0.1  -1.1]
-# t = [i + j - 1 for i=1:size(p, 1)-2, j=1:3]
-# tplot(p, t)
-# axis("equal")
-# ylim(-1.2, 0.1)
-# savefig("scratch/images/mesh.png")
-# println("scratch/images/mesh.png")
+function get_pte_dg(p, t, e, cols)
+    p_dg = [0.0 0.0]
+    t_dg = zeros(Int64, size(t))
+    e_dg = [0]
+    # all the nodes within each column will have a unique tag
+    for col ∈ cols
+        # current size of p_dg
+        np = size(p_dg, 1) - 1
 
-g = FEGrid("meshes/valign2D/mesh0.h5", 1)
-cols = get_cols(g.p, g.t)
-fig, ax, im = tplot(g)
-ax.axis("equal")
-cycle = [1, 2, 3, 1]
-for i ∈ eachindex(cols)
-    color = "C$(i-1)"
-    for k ∈ cols[i]
-        ax.plot(g.p[g.t[k, cycle], 1], g.p[g.t[k, cycle], 2], c=color, lw=0.5)
+        # new nodes in column
+        t_col = t[col, :]
+        nodes = sort(unique(t_col))
+
+        # add them to p_dg
+        p_dg = [p_dg; p[nodes, :]]
+
+        # mapping for new global node indices for each element
+        tmap(i) = np + searchsorted(nodes, i).start
+        for k ∈ col
+            t_dg[k, :] = tmap.(t[k, :])
+        end
+
+        # add nodes that were on the edge to e_dg
+        edge_nodes = np .+ findall(i -> nodes[i] ∈ e, 1:size(nodes, 1))
+        e_dg = [e_dg; edge_nodes]
     end
+    return p_dg[2:end, :], t_dg, e_dg[2:end]
 end
-savefig("scratch/images/cols.png")
-println("scratch/images/cols.png")
-plt.close()
 
-g = FEGrid("meshes/valign2D/mesh0.h5", 1)
+# g = FEGrid("meshes/valign2D/mesh0.h5", 1)
+# cols = get_cols(g.p, g.t)
+# fig, ax, im = tplot(g)
+# ax.axis("equal")
+# cycle = [1, 2, 3, 1]
+# for i ∈ eachindex(cols)
+#     color = "C$(i-1)"
+#     for k ∈ cols[i]
+#         ax.plot(g.p[g.t[k, cycle], 1], g.p[g.t[k, cycle], 2], c=color, lw=0.5)
+#     end
+# end
+# savefig("scratch/images/cols.png")
+# println("scratch/images/cols.png")
+# plt.close()
+
+# g = FEGrid("meshes/valign2D/mesh0.h5", 1)
+# cols = get_cols(g.p, g.t)
+# p_dg, t_dg, e_dg = get_pte_dg(g.p, g.t, g.e, cols)
+# fig, ax, im = tplot(g)
+# ax.axis("equal")
+# cycle = [1, 2, 3, 1]
+# for i ∈ eachindex(cols)
+#     color = "C$(i-1)"
+#     for k ∈ cols[i]
+#         ax.plot(p_dg[t_dg[k, cycle], 1], p_dg[t_dg[k, cycle], 2], c=color, lw=0.5)
+#         savefig("scratch/images/debug.png")
+#         sleep(0.1)
+#     end
+# end
+# savefig("scratch/images/debug.png")
+# println("scratch/images/debug.png")
+# plt.close()
+
+g = FEGrid("meshes/valign2D/mesh5.h5", 1)
 solve_toyDG1D(g)
