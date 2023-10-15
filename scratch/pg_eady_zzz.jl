399db1808362dd6f5c848fc7daca21b15bcfea46
Henry Peterson <hgpeterson@caltech.edu>
2023-10-15 16:37:49 -0700

    seems sensitive to grid

--- scratch/pg_eady_zzz.jl
+++ scratch/pg_eady_zzz.jl
@@ -19,16 +19,94 @@ function basic_state(z, Ri)
     return U, V, Bz, Uz, Vz
 end
 
+function basic_state(z, μ, ε²)
+    nz = length(z)
+    N = 3*nz # U, V, Bz
+    imap = reshape(1:N, (3, nz)) 
+    Umap = imap[1, :]
+    Vmap = imap[2, :]
+    Bzmap = imap[3, :]
+    A = Tuple{Int64,Int64,Float64}[]
+    r = zeros(N)
+    for i=2:nz-1
+        fd_z  = mkfdstencil(z[i-1:i+1], z[i], 1)
+        fd_zz = mkfdstencil(z[i-1:i+1], z[i], 2)
+
+        eq = imap[1, i]
+        push!(A, (eq, Umap[i-1], -ε²*fd_zz[1]))
+        push!(A, (eq, Umap[i],   -ε²*fd_zz[2]))
+        push!(A, (eq, Umap[i+1], -ε²*fd_zz[3]))
+        push!(A, (eq, Vmap[i],   -1))
+        
+        eq = imap[2, i]
+        push!(A, (eq, Vmap[i-1], -ε²*fd_zz[1]))
+        push!(A, (eq, Vmap[i],   -ε²*fd_zz[2]))
+        push!(A, (eq, Vmap[i+1], -ε²*fd_zz[3]))
+        push!(A, (eq, Umap[i],    1))
+        r[eq] = z[i]
+
+        eq = imap[3, i]
+        push!(A, (eq, Bzmap[i-1], -ε²/μ*fd_z[1]))
+        push!(A, (eq, Bzmap[i],   -ε²/μ*fd_z[2]))
+        push!(A, (eq, Bzmap[i+1], -ε²/μ*fd_z[3]))
+        push!(A, (eq, Vmap[i],    -1))
+    end
+
+    fd_z = mkfdstencil(z[1:3], z[1], 1)
+
+    eq = imap[1, 1]
+    push!(A, (eq, Umap[1], fd_z[1]))
+    push!(A, (eq, Umap[2], fd_z[2]))
+    push!(A, (eq, Umap[3], fd_z[3]))
+
+    eq = imap[2, 1]
+    push!(A, (eq, Vmap[1], fd_z[1]))
+    push!(A, (eq, Vmap[2], fd_z[2]))
+    push!(A, (eq, Vmap[3], fd_z[3]))
+
+    eq = imap[3, 1]
+    push!(A, (eq, Bzmap[1], 1))
+
+    fd_z  = mkfdstencil(z[nz-2:nz], z[nz], 1)
+
+    eq = imap[1, nz]
+    push!(A, (eq, Umap[nz-2], fd_z[1]))
+    push!(A, (eq, Umap[nz-1], fd_z[2]))
+    push!(A, (eq, Umap[nz],   fd_z[3]))
+
+    eq = imap[2, nz]
+    push!(A, (eq, Vmap[nz-2], fd_z[1]))
+    push!(A, (eq, Vmap[nz-1], fd_z[2]))
+    push!(A, (eq, Vmap[nz],   fd_z[3]))
+
+    eq = imap[3, nz]
+    push!(A, (eq, Bzmap[nz], 1))
+
+    A = sparse((x->x[1]).(A), (x->x[2]).(A), (x->x[3]).(A), N, N)
+    sol = A\r
+    U = sol[Umap]
+    V = sol[Vmap]
+    Bz = sol[Bzmap]
+    Uz = differentiate(U, z)
+    Vz = differentiate(V, z)
+    return U, V, Bz, Uz, Vz
+end
+
 function growth_rate(eigenvals)
     isort = sortperm(imag(eigenvals), rev=true)
     for i ∈ eachindex(isort)
         if !isnan(eigenvals[isort[i]])
+            # return isort[i+1], imag(eigenvals[isort[i+1]])
             return isort[i], imag(eigenvals[isort[i]])
         end
     end
 end
 
 function build_A_B!(A, B, k, l, z, μ, ε², U, V, Bz, Uz, Vz)
+    if primitive
+        Uzz = differentiate(Uz, z)
+        Vzz = differentiate(Vz, z)
+    end
     nz = length(z)
     N = 4*nz # u, v, w, b
     imap = reshape(1:N, (4, nz)) 
@@ -39,26 +117,47 @@ function build_A_B!(A, B, k, l, z, μ, ε², U, V, Bz, Uz, Vz)
     A[:] .= 0
     B[:] .= 0
     for i=2:nz-1
-        fd_z   = mkfdstencil(z[i-1:i+1], z[i], 1)
-        fd_zz  = mkfdstencil(z[i-1:i+1], z[i], 2)
+        fd_z  = mkfdstencil(z[i-1:i+1], z[i], 1)
+        fd_zz = mkfdstencil(z[i-1:i+1], z[i], 2)
         if i == 2
-            zzz_idxs = i-1:i+3
+            zzz_idxs = i:i+4
         elseif i == nz-1
-            zzz_idxs = i-3:i+1
+            zzz_idxs = i-4:i
         else
             zzz_idxs = i-2:i+2
         end
         fd_zzz = mkfdstencil(z[zzz_idxs], z[i], 3)
+        # fd_zzzz = mkfdstencil(z[zzz_idxs], z[i], 4)
 
         eq = imap[1, i]
+        if primitive 
+            A[eq, umap[i]] += im*k*Uz[i] + im*l*Vz[i]
+            A[eq, umap[i-1:i+1]] .+= (im*k*U[i] + im*l*V[i])*fd_z
+            A[eq, wmap[i]] += Uzz[i]
+            A[eq, wmap[i-1:i+1]] .+= Uz[i]*fd_z
+            B[eq, umap[i-1:i+1]] .+= im*fd_z
+        end
         A[eq, vmap[i-1:i+1]] .+= -fd_z
         A[eq, bmap[i]] += im*k
         A[eq, umap[zzz_idxs]] .+= -ε²*fd_zzz
+        # A[eq, vmap[i-1:i+1]] .+= -fd_zz
+        # A[eq, bmap[i-1:i+1]] .+= im*k*fd_z
+        # A[eq, umap[zzz_idxs]] .+= -ε²*fd_zzzz
         
         eq = imap[2, i]
+        if primitive 
+            A[eq, vmap[i]] += im*k*Uz[i] + im*l*Vz[i]
+            A[eq, vmap[i-1:i+1]] .+= (im*k*U[i] + im*l*V[i])*fd_z
+            A[eq, wmap[i]] += Vzz[i]
+            A[eq, wmap[i-1:i+1]] .+= Vz[i]*fd_z
+            B[eq, vmap[i-1:i+1]] .+= im*fd_z
+        end
         A[eq, umap[i-1:i+1]] .+= fd_z
         A[eq, bmap[i]] += im*l
         A[eq, vmap[zzz_idxs]] .+= -ε²*fd_zzz
+        # A[eq, umap[i-1:i+1]] .+= fd_zz
+        # A[eq, bmap[i-1:i+1]] .+= im*l*fd_z
+        # A[eq, vmap[zzz_idxs]] .+= -ε²*fd_zzzz
 
         eq = imap[3, i]
         A[eq, umap[i]] += im*k
@@ -76,12 +175,16 @@ function build_A_B!(A, B, k, l, z, μ, ε², U, V, Bz, Uz, Vz)
     fd_z = mkfdstencil(z[1:3], z[1], 1)
     A[imap[1, 1], umap[1]] += 1
     A[imap[2, 1], vmap[1]] += 1
+    # A[imap[1, 1], umap[1:3]] .+= fd_z
+    # A[imap[2, 1], vmap[1:3]] .+= fd_z
     A[imap[3, 1], wmap[1]] += 1
     A[imap[4, 1], bmap[1:3]] .+= fd_z
 
     fd_z  = mkfdstencil(z[nz-2:nz], z[nz], 1)
     A[imap[1, nz], umap[nz]] += 1
     A[imap[2, nz], vmap[nz]] += 1
+    # A[imap[1, nz], umap[nz-2:nz]] .+= fd_z
+    # A[imap[2, nz], vmap[nz-2:nz]] .+= fd_z
     A[imap[3, nz], wmap[nz]] += 1
     A[imap[4, nz], bmap[nz-2:nz]] .+= fd_z
 
@@ -95,14 +198,15 @@ function build_A_B(k, l, z, μ, ε², U, V, Bz, Uz, Vz)
 end
 
 function compute_σ_grid(z, μ, ε², Ri)
-    U, V, Bz, Uz, Vz = basic_state(z, Ri)
+    # U, V, Bz, Uz, Vz = basic_state(z, Ri)
+    U, V, Bz, Uz, Vz = basic_state(z, μ, ε²)
     n = 2^4
     kmax = 3
     lmax = 10
     k = 0:kmax/(n-1):kmax
     l = -lmax:2lmax/(n-1):lmax
     σ = zeros(length(l), length(k))
-    N = 5*length(z)
+    N = 4*length(z)
     A = zeros(ComplexF64, N, N)
     B = zeros(ComplexF64, N, N)
     @showprogress for i ∈ eachindex(k)
@@ -136,6 +240,7 @@ end
 function plot_unstable_mode(k, l, z, μ, ε², Ri)
     # compute 
     U, V, Bz, Uz, Vz = basic_state(z, Ri)
+    # U, V, Bz, Uz, Vz = basic_state(z, μ, ε²)
     A, B = build_A_B(k, l, z, μ, ε², U, V, Bz, Uz, Vz)
     println(cond(A))
     println(cond(B))
@@ -230,21 +335,44 @@ function xz_plot(xx, zz, uu, ax, label)
     ax.spines["bottom"].set_visible(false)
 end
 
+function plot_basic_state(z, μ)
+    fig, ax = plt.subplots(1, 3, figsize=(6, 3.2), sharey=true)
+    for ε² = [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1]
+        U, V, Bz, Uz, Vz = basic_state(z, μ, ε²)
+        ax[1].plot(U, z, label=ε²)
+        ax[2].plot(V, z)
+        ax[3].plot(Bz, z)
+    end
+    ax[1].set_xlim(-0.6, 0.6)
+    ax[1].set_xticks(-0.6:0.2:0.6)
+    ax[2].set_xlim(-0.25, 0.25)
+    ax[2].set_xticks(-0.2:0.1:0.2)
+    ax[1].legend()
+    ax[1].set_ylabel(L"z")
+    ax[1].set_xlabel(L"\bar u")
+    ax[2].set_xlabel(L"\bar v")
+    ax[3].set_xlabel(L"\partial_z \bar b")
+    savefig("images/basic_state.png")
+    println("images/basic_state.png")
+    plt.close()
+end
+
 function main()
     nz = 2^6
-    # z = -0.5:1/(nz-1):0.5
-    z = -cos.(π*(0:nz-1)/(nz-1))/2
+    z = collect(-0.5:1/(nz-1):0.5)
+    # z = -cos.(π*(0:nz-1)/(nz-1))/2
     μ = 1
-    ε² = 1e-2
+    ε² = 1e-3
     Ri = 1
 
-    # plot_basic_state(z, Ri)
+    # plot_basic_state(z, μ)
 
-    # k, l, σ = compute_σ_grid(z, μ, ε², Ri)
+    k, l, σ = compute_σ_grid(z, μ, ε², Ri)
 
-    plot_unstable_mode(0.5, -1, z, μ, ε², Ri)
+    # plot_unstable_mode(1, -4, z, μ, ε², Ri)
 
     return
 end
 
+primitive = false
 main()
