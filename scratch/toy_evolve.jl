0c31a3ca24916d7406760cad0395dfc167fd3025
hgpeterson <hgpeterson@caltech.edu>
2024-04-02 14:29:56 -0700

    gpu test

--- scratch/toy_evolve.jl
+++ scratch/toy_evolve.jl
@@ -5,12 +5,14 @@ using Random
 using HDF5
 using SparseArrays
 using Printf
+using CUDA
+using CUDA.CUSPARSE
 
 plt.style.use("../plots.mplstyle")
 plt.close("all")
 pygui(false)
 
-set_out_folder("../output/delta2.5")
+set_out_folder("../output")
 if !isdir("$out_folder/data")
     mkdir("$out_folder/data")
 end
@@ -78,41 +80,132 @@ function build_advection_arrays(g, δ)
         end
     end
 
-    return A1, A2, A3
+    return CuArray(A1), CuArray(A2), CuArray(A3)
 end
 
-function advection(A1, A2, q, ψ)
-    g = ψ.g
-    adv = zeros(g.np)
-    for k ∈ 1:g.nt, i ∈ 1:g.nn, iψ1 ∈ 1:g.nn, iq ∈ 1:g.nn
-        adv[g.t[k, i]] += A1[k, i, iψ1, iq]*ψ[g.t[k, iψ1]]*q[g.t[k, iq]]
-        for iψ2 ∈ 1:g.nn
-            adv[g.t[k, i]] += A2[k, i, iψ1, iψ2, iq]*ψ[g.t[k, iψ1]]*ψ[g.t[k, iψ2]]*q[g.t[k, iq]]
+# function advection(A1, A2, q, ψ)
+#     g = ψ.g
+#     adv = zeros(g.np)
+#     for k ∈ 1:g.nt, i ∈ 1:g.nn, iψ1 ∈ 1:g.nn, iq ∈ 1:g.nn
+#         adv[g.t[k, i]] += A1[k, i, iψ1, iq]*ψ[g.t[k, iψ1]]*q[g.t[k, iq]]
+#         for iψ2 ∈ 1:g.nn
+#             adv[g.t[k, i]] += A2[k, i, iψ1, iψ2, iq]*ψ[g.t[k, iψ1]]*ψ[g.t[k, iψ2]]*q[g.t[k, iq]]
+#         end
+#     end
+#     return adv
+# end
+function advection(A1, A2, q, ψ, t)
+    # unpack
+    g = q.g
+
+    # load arrays on GPU
+    adv = CUDA.zeros(g.nt, g.nn) 
+    q_gpu = CuArray(q.values)
+    ψ_gpu = CuArray(ψ.values)
+
+    # setup advection kernel
+    kernel = @cuda launch=false gpu_adv!(adv, A1, A2, q_gpu, ψ_gpu, t)
+    config = launch_configuration(kernel.fun)
+    threads = min(g.nt, config.threads)
+    blocks = cld(g.nt, threads)
+
+    CUDA.@sync begin
+        kernel(adv, A1, A2, q_gpu, ψ_gpu, t; threads, blocks)
+    end
+
+    # copy result to CPU
+    cpu_adv = Array(adv)
+    return cpu_adv
+end
+
+function gpu_adv!(adv, A1, A2, q, ψ, t)
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
+    for k ∈ index:stride:size(A1, 1), i ∈ axes(A1, 2), iψ1 ∈ axes(A1, 3), iq ∈ axes(A1, 4)
+        adv[k, i] += A1[k, i, iψ1, iq]*ψ[t[k, iψ1]]*q[t[k, iq]]
+        for iψ2 ∈ axes(A2, 4)
+            adv[k, i] += A2[k, i, iψ1, iψ2, iq]*ψ[t[k, iψ1]]*ψ[t[k, iψ2]]*q[t[k, iq]]
         end
     end
-    return adv
+    return
 end
 
-function build_M_SD(A3, q, ψ)
-    g = ψ.g
-    M_SD_I = zeros(Int64,   g.nt*g.nn^3)
-    M_SD_J = zeros(Int64,   g.nt*g.nn^3)
-    M_SD_V = zeros(Float64, g.nt*g.nn^3)
+# function gpu_adv!(adv, δ, w, φ, φx, φy, Δ, ψ, q, t)
+#     index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+#     stride = gridDim().x * blockDim().x
+#     for k ∈ index:stride:length(Δ), i ∈ axes(q, 2), iψ1 ∈ axes(ψ, 2), iq ∈ axes(q, 2), i_quad ∈ eachindex(w)
+#         adv[k, i] += w[i_quad]*(φx[k, iψ1, i_quad]*φy[k, iq, i_quad] - φy[k, iψ1, i_quad]*φx[k, iq, i_quad])*φ[i, i_quad]*Δ[k]*ψ[t[k, iψ1]]*q[t[k, iq]]
+#         for iψ2 ∈ axes(ψ, 2)
+#             adv[k, i] += δ[k]*w[i_quad]*(φx[k, iψ1, i_quad]*φy[k, iq, i_quad] - φy[k, iψ1, i_quad]*φx[k, iq, i_quad])*
+#                                         (φx[k, iψ2, i_quad]*φy[k, i,  i_quad] - φy[k, iψ2, i_quad]*φx[k, i,  i_quad])*Δ[k]*ψ[t[k, iψ1]]*ψ[t[k, iψ2]]*q[t[k, iq]]
+#         end
+#     end
+#     return
+# end
+
+# function build_M_SD(A3, q, ψ)
+#     g = ψ.g
+#     M_SD_I = zeros(Int64,   g.nt*g.nn^3)
+#     M_SD_J = zeros(Int64,   g.nt*g.nn^3)
+#     M_SD_V = zeros(Float64, g.nt*g.nn^3)
+#     n = 1
+#     for k ∈ 1:g.nt, i ∈ 1:g.nn, iψ ∈ 1:g.nn, iq ∈ 1:g.nn
+#         M_SD_I[n] = g.t[k, i]
+#         M_SD_J[n] = g.t[k, iq]
+#         M_SD_V[n] = A3[k, i, iψ, iq]*ψ[g.t[k, iψ]]*q[g.t[k, iq]]
+#         n += 1
+#     end
+#     return sparse(M_SD_I, M_SD_J, M_SD_V)
+# end
+function build_M_SD(M_SD_I, M_SD_J, A3, q, ψ, t)
+    # unpack
+    g = q.g
+
+    # load arrays on GPU
+    M_SD_V = CUDA.zeros(g.nt, g.nn, g.nn) 
+    ψ_gpu = CuArray(ψ.values)
+    q_gpu = CuArray(q.values)
+
+    # setup kernel
+    kernel = @cuda launch=false gpu_M_SD!(M_SD_V, A3, q_gpu, ψ_gpu, t)
+    config = launch_configuration(kernel.fun)
+    threads = min(g.nt, config.threads)
+    blocks = cld(g.nt, threads)
+
+    CUDA.@sync begin
+        kernel(M_SD_V, A3, q_gpu, ψ_gpu, t; threads, blocks)
+    end
+
+    # sparse matrix
+    return CuSparseMatrixCSC(M_SD_I, M_SD_J, M_SD_V[:], (g.np, g.np))
+end
+function build_M_SD_IJ(g)
+    M_SD_I = zeros(Int32, g.nt*g.nn*g.nn) 
+    M_SD_J = zeros(Int32, g.nt*g.nn*g.nn) 
     n = 1
-    for k ∈ 1:g.nt, i ∈ 1:g.nn, iψ ∈ 1:g.nn, iq ∈ 1:g.nn
+    for k ∈ 1:g.nt, i ∈ 1:g.nn, iq ∈ 1:g.nn
         M_SD_I[n] = g.t[k, i]
         M_SD_J[n] = g.t[k, iq]
-        M_SD_V[n] = A3[k, i, iψ, iq]*ψ[g.t[k, iψ]]*q[g.t[k, iq]]
         n += 1
     end
-    return sparse(M_SD_I, M_SD_J, M_SD_V)
+    M_SD_I = CuArray(M_SD_I)
+    M_SD_J = CuArray(M_SD_J)
+    return M_SD_I, M_SD_J
+end
+function gpu_M_SD!(M_SD_V, A3, q, ψ, t)
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = gridDim().x * blockDim().x
+    for k ∈ index:stride:size(A3, 1), i ∈ axes(A3, 2), iψ ∈ axes(A3, 3), iq ∈ axes(A3, 4)
+        M_SD_V[k, i, iq] += A3[k, i, iψ, iq]*ψ[t[k, iψ]]*q[t[k, iq]]
+    end
+    return 
 end
 
 function save_q(q, filename)
-    file = h5open(filename, "w")
-    write(file, "q", q.values)
-    close(file)
-    println(filename)
+    # file = h5open(filename, "w")
+    # write(file, "q", q.values)
+    # close(file)
+    # println(filename)
 end
 
 function read_q(filename)
@@ -130,6 +223,8 @@ function evolve()
     # grid
     # g = Grid(Triangle(order=1), "../meshes/circle/mesh4.h5")
     g = Grid(Triangle(order=1), "../meshes/H/mesh4.h5")
+    t_gpu = CuArray(g.t)
+    el_map = nuPGCM.build_element_map(g)
 
     # δ = const*(local mesh width)
     h = sqrt.(g.J.dets)*2/3^(1/4)
@@ -138,7 +233,9 @@ function evolve()
     # matrices
     K = nuPGCM.stiffness_matrix(g)
     M = nuPGCM.mass_matrix(g)
-    Pinv = sparse(inv(Diagonal(M)))
+    M_gpu = CuSparseMatrixCSC(M)
+    M_SD_I, M_SD_J = build_M_SD_IJ(g)
+    Pinv = CuSparseMatrixCSC(sparse(inv(Diagonal(M))))
     inv_LHS = build_inversion_LHS(g, K, M, λ)
     A1, A2, A3 = build_advection_arrays(g, δ)
 
@@ -147,43 +244,45 @@ function evolve()
     y = g.p[:, 2]
     # Δ = 0.1
     # q = @. exp(-(x + 0.25)^2/(2*Δ^2) - y^2/(2*Δ^2)) - exp(-(x - 0.25)^2/(2*Δ^2) - y^2/(2*Δ^2))
-    # q = @. sin(10*π*y)*sin(10*π*x)
-    # q .+= 0.05*(rand(g.np) .- 0.5)
-    # i_img = 0
-    i_img = 195
-    q = read_q(@sprintf("%s/data/q%03d.h5", out_folder, i_img))
+    q = @. sin(10*π*y)*sin(10*π*x)
+    q .+= 0.05*(rand(g.np) .- 0.5)
+    i_img = 0
+    # q = read_q(@sprintf("%s/data/q%03d.h5", out_folder, i_img))
     qmax = 1.0
     q = FEField(q, g)
     ψ = FEField(0, g)
     invert!(ψ, inv_LHS, M, q)
     quick_plot(q, filename=@sprintf("%s/images/q%03d.png", out_folder, i_img), vmax=qmax)
-    # save_q(q, @sprintf("%s/data/q%03d.h5", out_folder, i_img))
+    save_q(q, @sprintf("%s/data/q%03d.h5", out_folder, i_img))
     i_img += 1
 
     # step forward
     t1 = time()
-    N = 100000
-    dq = zeros(g.np)
-    dq_prev = zeros(g.np)
+    N = 100
+    dq = CUDA.zeros(eltype(M_gpu), g.np) # pre-allocate for `cg!`
+    dq_prev = zeros(eltype(M_gpu), g.np) 
     for i ∈ 1:N
         # update flow
         invert!(ψ, inv_LHS, M, q)
 
         # compute dq
-        adv = advection(A1, A2, q.values, ψ)
-        M_SD = build_M_SD(A3, q.values, ψ)
-        nuPGCM.cg!(dq, M + M_SD, adv; Pinv)
+        @time begin 
+            adv_el = advection(A1, A2, q, ψ, t_gpu)
+            adv = CuArray(el_map*adv_el[:])
+            M_SD = build_M_SD(M_SD_I, M_SD_J, A3, q, ψ, t_gpu)
+        end
+        nuPGCM.cg!(dq, M_gpu + M_SD, adv; Pinv) # 0.4
 
         if i == 1
             # euler first step
-            q.values[:] = q.values - Δt*dq
+            q.values[:] = q.values - Δt*Array(dq)
         else
             # AB2 otherwise
-            q.values[:] = q.values - 3/2*Δt*dq + 1/2*Δt*dq_prev
+            q.values[:] = q.values - 3/2*Δt*Array(dq) + 1/2*Δt*dq_prev
         end
 
         # save for next step
-        dq_prev[:] = dq[:]
+        dq_prev[:] = Array(dq)[:]
 
         if mod(i, 100) == 0
             # CFL
