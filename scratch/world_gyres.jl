1bf0c298e6659b65269a0e4c01179a9c497733f0
hgpeterson <hgpeterson@caltech.edu>
2024-04-09 13:29:30 -0700

    lat lon

--- scratch/world_gyres.jl
+++ scratch/world_gyres.jl
@@ -51,8 +51,8 @@ function build_barotropic_LHS(r, H)
     A = Tuple{Int64,Int64,Float64}[]
     for k=1:g.nt, i=1:el.n, j=1:el.n
         if g.t[k, i] ∉ bdy 
-            push!(A, (g.t[k, i], g.t[k, j], nuPGCM.ref_el_quad(ξ -> ∫K(ξ, i, j, k), el) +
-                                            nuPGCM.ref_el_quad(ξ -> ∫C(ξ, i, j, k), el)))
+            push!(A, (g.t[k, i], g.t[k, j], nuPGCM.ref_el_quad(ξ -> ∫K(ξ, i, j, k), el)))# +
+                                            # nuPGCM.ref_el_quad(ξ -> ∫C(ξ, i, j, k), el)))
         end
     end
 
@@ -97,43 +97,67 @@ function build_barotropic_RHS(τx, τy, H, ρ)
     return rhs
 end
 
-g = Grid(Triangle(order=1), "../meshes/ocean_a.h5")
-H = FEField(4e3, g)
-r = FEField(1e-5, g)
-ϕ(x) = atan(x[3]/sqrt(x[1]^2 + x[2]^2))
-τx = FEField(x -> -0.1*cos(6/2*ϕ(x)), g)
-τy = FEField(0, g)
-ρ = 1000
-LHS = build_barotropic_LHS(r, H)
+# g = Grid(Triangle(order=1), "../meshes/ocean_a.h5")
+# H = FEField(4e3, g)
+# r = FEField(1e-5, g)
+# ϕ(x) = atan(x[3]/sqrt(x[1]^2 + x[2]^2))
+# θ(x) = atan(x[2], x[1])
+# τx = FEField(x -> -0.1*cos(6/2*ϕ(x)), g)
+# τy = FEField(0, g)
+# ρ = 1000
+# LHS = build_barotropic_LHS(r, H)
 # M = nuPGCM.mass_matrix(g)
 # RHS = M*ones(g.np)
 # RHS[g.e["bdy"]] .= 0
-RHS = build_barotropic_RHS(τx, τy, H, ρ)
-Ψ = LHS\RHS
+# # RHS = build_barotropic_RHS(τx, τy, H, ρ)
+# Ψ = LHS\RHS
 
-p = g.p
-np = g.np
-t = g.t
-nt = g.nt
-e = g.e["bdy"]
-i1 = findall(i -> p[i, 1] ≥ 0, 1:np)
-i2 = findall(i -> p[i, 1] ≤ 0, 1:np)
-t1 = t[findall(k -> t[k, 1] ∈ i1 && t[k, 2] ∈ i1 && t[k, 3] ∈ i1, 1:nt), :]
-e1 = e[findall(i -> e[i] ∈ i1, 1:size(e, 1))]
-t2 = t[findall(k -> t[k, 1] ∈ i2 && t[k, 2] ∈ i2 && t[k, 3] ∈ i2, 1:nt), :]
-e2 = e[findall(i -> e[i] ∈ i2, 1:size(e, 1))]
-fig, ax = plt.subplots(1, 2, figsize=(6.4, 2))
-nuPGCM.tplot(p[:, 2:3], t1, Ψ; fig, ax=ax[1], contour=true)
-# nuPGCM.tplot(p, t1, τ.values; fig, ax=ax[1])
-p_flip = copy(p)
-p_flip[:, 2] = -p_flip[:, 2]
-nuPGCM.tplot(p_flip[:, 2:3], t2, Ψ; fig, ax=ax[2], contour=true)
-# nuPGCM.tplot(p_flip, t2, τ.values; fig, ax=ax[2])
-ax[1].axis("equal")
-ax[2].axis("equal")
-# ax[1].set_xticks([])
-# ax[1].set_yticks([])
-# ax[2].set_xticks([])
-# ax[2].set_yticks([])
-savefig("$out_folder/images/psi.png")
-println("$out_folder/images/psi.png")
+# p = g.p
+# np = g.np
+# t = g.t
+# nt = g.nt
+# e = g.e["bdy"]
+# i1 = findall(i -> p[i, 1] ≥ 0, 1:np)
+# i2 = findall(i -> p[i, 1] ≤ 0, 1:np)
+# t1 = t[findall(k -> t[k, 1] ∈ i1 && t[k, 2] ∈ i1 && t[k, 3] ∈ i1, 1:nt), :]
+# e1 = e[findall(i -> e[i] ∈ i1, 1:size(e, 1))]
+# t2 = t[findall(k -> t[k, 1] ∈ i2 && t[k, 2] ∈ i2 && t[k, 3] ∈ i2, 1:nt), :]
+# e2 = e[findall(i -> e[i] ∈ i2, 1:size(e, 1))]
+# fig, ax = plt.subplots(1, 2, figsize=(6.4, 2))
+# nuPGCM.tplot(p[:, 2:3], t1, Ψ; fig, ax=ax[1], contour=true)
+# # nuPGCM.tplot(p, t1, τ.values; fig, ax=ax[1])
+# p_flip = copy(p)
+# p_flip[:, 2] = -p_flip[:, 2]
+# nuPGCM.tplot(p_flip[:, 2:3], t2, Ψ; fig, ax=ax[2], contour=true)
+# # nuPGCM.tplot(p_flip, t2, τ.values; fig, ax=ax[2])
+# ax[1].axis("equal")
+# ax[2].axis("equal")
+# # ax[1].set_xticks([])
+# # ax[1].set_yticks([])
+# # ax[2].set_xticks([])
+# # ax[2].set_yticks([])
+# savefig("$out_folder/images/psi.png")
+# println("$out_folder/images/psi.png")
+
+θs = [θ(p[i, :])*180/π for i ∈ 1:np]
+ϕs = [ϕ(p[i, :])*180/π for i ∈ 1:np]
+pθϕ = hcat(θs, ϕs)
+tθϕ = copy(t)
+for i ∈ 1:nt
+    if !(sign(θs[t[i, 1]]) == sign(θs[t[i, 2]]) == sign(θs[t[i, 3]])) && abs(θs[t[i, 1]]) > 170
+        println("Longitude sign change in triangle $i")
+        tθϕ[i, :] = t[1, :]
+    end
+end
+tθϕ = unique(tθϕ, dims=1)
+ntθϕ = size(tθϕ, 1)
+println("Number of triangles: $ntθϕ")
+# fig, ax, im = nuPGCM.tplot(pθϕ, tθϕ)
+fig, ax, im = nuPGCM.tplot(pθϕ, tθϕ, Ψ; contour=true)
+ax.plot(pθϕ[e, 1], pθϕ[e, 2], "ko", ms=0.3, markeredgecolor="none")
+ax.set_xlabel("Longitude (°)")
+ax.set_ylabel("Latitude (°)")
+ax.set_xticks(-180:90:180)
+ax.set_yticks(-90:45:90)
+savefig("$out_folder/images/psi_latlon.png")
+println("$out_folder/images/psi_latlon.png")
