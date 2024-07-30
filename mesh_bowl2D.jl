1b9370d7f1284c39418c413b5d797968ba362b62
hgpeterson <hgpeterson@caltech.edu>
2024-07-29 17:00:00 -0700

    upgrade 2D code to gpu

--- mesh_bowl2D.jl
+++ mesh_bowl2D.jl
@@ -3,6 +3,7 @@ using Gridap
 using GridapGmsh
 using Gmsh: gmsh
 using PyPlot
+using Printf
 
 pygui(false)
 plt.style.use("plots.mplstyle")
@@ -13,71 +14,51 @@ function generate_bowl_mesh(h)
     gmsh.option.setNumber("General.Terminal", 1)
     gmsh.model.add("bowl2D")
 
-    # points
-    gmsh.model.geo.addPoint(-L, 0, 0, h)
-    gmsh.model.geo.addPoint(0, -2, 0, h) # control point
-    gmsh.model.geo.addPoint(L, 0, 0, h)
+    # # H = 1 - x^2
+    # gmsh.model.geo.addPoint(-1, 0, 0, h)
+    # gmsh.model.geo.addPoint(0, -2, 0, h) # control point
+    # gmsh.model.geo.addPoint(1, 0, 0, h)
+    # gmsh.model.geo.addBezier([1, 2, 3], 1) # bezier curve through (-1, 0), (0, -2), (1, 0) is a parabola with z = x^2 - 1
+    # gmsh.model.geo.addLine(3, 1, 2)
+    # gmsh.model.geo.addCurveLoop(1:2, 1)
+    # gmsh.model.geo.addPlaneSurface([1], 1)
+    # gmsh.model.geo.synchronize()
+    # gmsh.model.addPhysicalGroup(0, [1, 3], 1, "bot")
+    # gmsh.model.addPhysicalGroup(1, [1], 1, "bot")
+    # gmsh.model.addPhysicalGroup(1, [2], 2, "sfc")
+    # gmsh.model.addPhysicalGroup(2, [1], 3, "int")
 
-    # lines
-    gmsh.model.geo.addBezier([1, 2, 3], 1) # bezier curve through (-1, 0), (0, -2), (1, 0) is a parabola with z = x^2 - 1
-    gmsh.model.geo.addLine(3, 1, 2)
+    # H = √(2 - x^2) - 1
+    gmsh.model.occ.addDisk(0, 1, 0, √2, √2)
+    gmsh.model.occ.addRectangle(-4, 0, 0, 8, 8)
+    gmsh.model.occ.synchronize()
+    gmsh.model.occ.cut([(2, 1)], [(2, 2)])
+    gmsh.model.occ.synchronize()
+    # gmsh.model.addPhysicalGroup(0, 1:2, 1, "bot")
+    # gmsh.model.addPhysicalGroup(1, 1:2, 1, "bot")
+    # gmsh.model.addPhysicalGroup(2, [1], 1, "bot")
+    # gmsh.model.addPhysicalGroup(2, [2], 2, "sfc")
+    # gmsh.model.addPhysicalGroup(3, [3], 3, "int")
+    gmsh.model.mesh.setSize(gmsh.model.getEntities(0), h)
 
-    # loop curves together and define surface
-    gmsh.model.geo.addCurveLoop(1:2, 1)
-    gmsh.model.geo.addPlaneSurface([1], 1)
-    
-    # sync 
-    gmsh.model.geo.synchronize()
-
-    # define boundary and interior physical groups
-    gmsh.model.addPhysicalGroup(0, [1, 3], 1, "bot")
-    gmsh.model.addPhysicalGroup(1, [1], 1, "bot")
-    gmsh.model.addPhysicalGroup(1, [2], 2, "sfc")
-    gmsh.model.addPhysicalGroup(2, [1], 3, "int")
-
-    # # refine near boundary
-    # rf = 2
-    # Δ = 0.05
-    # gmsh.model.mesh.field.add("Distance", 1)
-    # gmsh.model.mesh.field.setNumbers(1, "CurvesList", [d[2] for d in gmsh.model.getEntities(1)])
-    # gmsh.model.mesh.field.setNumber(1, "Sampling", rf/h)
-    # # gmsh.model.mesh.field.add("Threshold", 2)
-    # # gmsh.model.mesh.field.setNumber(2, "InField", 1)
-    # # gmsh.model.mesh.field.setNumber(2, "SizeMin", h/4)
-    # # gmsh.model.mesh.field.setNumber(2, "SizeMax", h)
-    # # gmsh.model.mesh.field.setNumber(2, "DistMin", h)
-    # # gmsh.model.mesh.field.setNumber(2, "DistMax", 4h)
-    # gmsh.model.mesh.field.add("MathEval", 2)
-    # gmsh.model.mesh.field.setString(2, "F", "$h*(1 - $((rf - 1)/rf)*exp(-F1/$Δ))")
-    # gmsh.model.mesh.field.setAsBackgroundMesh(2)
-    # gmsh.option.setNumber("Mesh.MeshSizeExtendFromBoundary", 0)
-    # gmsh.option.setNumber("Mesh.MeshSizeFromPoints", 0)
-    # gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 0)
-    # gmsh.option.setNumber("Mesh.Algorithm", 5)
-
-    # generate mesh
+    # generate and save
     gmsh.model.mesh.generate(2)
-
-    # save
-    gmsh.write("bowl2D.msh")
+    gmsh.write(@sprintf("bowl2D_%0.2f.msh", h))
     gmsh.finalize()
 end
 
 function mesh_plot(p, t)
     fig, ax = plt.subplots(1)
     ax.tripcolor(p[:, 1], p[:, 2], t .- 1, 0*t[:, 1], cmap="Greys", edgecolors="k", lw=0.2, rasterized=true)
-    # ax.plot(-1:0.01:1, (-1:0.01:1).^2 .- 1, "r")
     ax.axis("equal")
-    # ax.set_xlim(-L, -L+1.0)
-    # ax.set_ylim(-0.1, 0)
     ax.spines["left"].set_visible(false)
     ax.spines["bottom"].set_visible(false)
-    savefig("images/bowl2D.png")
-    println("images/bowl2D.png")
+    savefig("bowl2D.png")
+    println("bowl2D.png")
     plt.close()
 end
 
-L = 1
-generate_bowl_mesh(0.02)
-p, t = get_p_t("bowl2D.msh")
+h = 0.01
+generate_bowl_mesh(h)
+p, t = get_p_t(@sprintf("bowl2D_%0.2f.msh", h))
 mesh_plot(p, t)
