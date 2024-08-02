a42014604b9c06e4b57a7e304d12eef89852c584
Henry Peterson <hgpeterson@caltech.edu>
2024-08-02 10:49:18 -0700

    still trying to make parabolic bowl in 3D that works with gridap

--- mesh_bowl3D.jl
+++ mesh_bowl3D.jl
@@ -66,53 +66,83 @@ function generate_bowl_mesh(h)
     # gmsh.write("bowl3D.msh")
     # gmsh.finalize()
 
-    # init
+    # # init
+    # gmsh.initialize()
+
+    # # model
+    # gmsh.model.add("bowl3D")
+
+    # # points
+    # gmsh.model.occ.addPoint(0, 0, -1, h, 1)
+    # gmsh.model.occ.addPoint(0.5, 0, -1, h, 2) # control point
+    # gmsh.model.occ.addPoint(1, 0, 0, h, 3)
+    # gmsh.model.occ.addPoint(0, 0, 0, h, 4)
+
+    # # curves
+    # gmsh.model.occ.addBezier([1, 2, 3], 1)
+    # gmsh.model.occ.addLine(3, 4, 2)
+
+    # # revolve curves 1 and 2 about the z-axis
+    # # ov = gmsh.model.occ.revolve([(1, 1), (1, 2)], 0, 0, -1, 0, 0, 1, 2π)
+    # ov = gmsh.model.occ.revolve([(1, 1), (1, 2)], 0, 0, -1, 0, 0, 1, π)
+    # display(ov)
+
+    # gmsh.model.occ.addCurveLoop([1, 2, 5, 6], 3)
+    # gmsh.model.occ.addSurfaceFilling(3, 3)
+
+    # # combined 2D surfaces define boundary of 3D volume
+    # # gmsh.model.occ.addSurfaceLoop([1, 2], 1)
+    # gmsh.model.occ.addSurfaceLoop(1:3, 1)
+    # gmsh.model.occ.addVolume([1], 1)
+
+    # # label physical groups
+    # gmsh.model.occ.synchronize()
+    # gmsh.model.addPhysicalGroup(0, [1, 3], 1, "bot")
+    # gmsh.model.addPhysicalGroup(0, [4], 2, "sfc")
+    # # gmsh.model.addPhysicalGroup(1, [1, 4], 1, "bot")
+    # # gmsh.model.addPhysicalGroup(1, [2], 2, "sfc")
+    # gmsh.model.addPhysicalGroup(1, [1, 4, 5], 1, "bot")
+    # gmsh.model.addPhysicalGroup(1, [2, 6], 2, "sfc")
+    # # gmsh.model.addPhysicalGroup(2, [1, 3], 1, "bot")
+    # gmsh.model.addPhysicalGroup(2, [1, 3, 4], 1, "bot")
+    # gmsh.model.addPhysicalGroup(2, [2], 2, "sfc")
+    # gmsh.model.addPhysicalGroup(3, [1], 3, "int")
+
+    # for i ∈ 0:3
+    #     println(gmsh.model.getEntities(i))
+    # end
+
+    # # generate mesh
+    # gmsh.model.mesh.generate(3)
+
+    # # save
+    # gmsh.write("bowl3D.msh")
+    # gmsh.finalize()
+
     gmsh.initialize()
-
-    # model
     gmsh.model.add("bowl3D")
-
-    # points
     gmsh.model.occ.addPoint(0, 0, -1, h, 1)
-    gmsh.model.occ.addPoint(0.5, 0, -1, h, 2) # control point
-    gmsh.model.occ.addPoint(1, 0, 0, h, 3)
-    gmsh.model.occ.addPoint(0, 0, 0, h, 4)
-
-    # label 0D physical groups
-    gmsh.model.occ.synchronize()
-    gmsh.model.addPhysicalGroup(0, [1, 3], 1, "bot")
-    gmsh.model.addPhysicalGroup(0, [4], 2, "sfc")
-
-    # curves
-    gmsh.model.occ.addBezier([1, 2, 3], 1)
-    gmsh.model.occ.addLine(3, 4, 2)
-
-    # revolve curves 1 and 2 about the z-axis
-    ov = gmsh.model.occ.revolve([(1, 1), (1, 2)], 0, 0, -1, 0, 0, 1, 2π)
-    display(ov)
-
-    # label 1D physical groups
-    gmsh.model.occ.synchronize()
-    gmsh.model.addPhysicalGroup(1, [1, 3, 4], 1, "bot")
-    gmsh.model.addPhysicalGroup(1, [2], 2, "sfc")
-
-    # label 2D physical groups
-    gmsh.model.occ.synchronize()
-    gmsh.model.addPhysicalGroup(2, [1], 1, "bot")
-    gmsh.model.addPhysicalGroup(2, [2], 2, "sfc")
-
-    # combined 2D surfaces define boundary of 3D volume
+    gmsh.model.occ.addPoint(1, 0, 0, h, 2)
+    gmsh.model.occ.addLine(1, 2, 1)
+    ov = gmsh.model.occ.revolve([(1, 1)], 0, 0, -1, 0, 0, 1, 2π)
+    gmsh.model.occ.addCurveLoop([3], 2)
+    gmsh.model.occ.addSurfaceFilling(2, 2)
     gmsh.model.occ.addSurfaceLoop([1, 2], 1)
     gmsh.model.occ.addVolume([1], 1)
-
-    # label 3D physical group
     gmsh.model.occ.synchronize()
-    gmsh.model.addPhysicalGroup(3, [1], 3, "int")
-
-    # generate mesh
+    gmsh.model.addPhysicalGroup(0, 1:2, 1)
+    gmsh.model.addPhysicalGroup(1, 1:3, 1)
+    gmsh.model.addPhysicalGroup(2, 1:3, 1)
+    gmsh.model.addPhysicalGroup(3, [1], 1)
+    for i ∈ 0:3
+        println(gmsh.model.getEntities(i))
+    end
     gmsh.model.mesh.generate(3)
-
-    # save
+    _, p, _ = gmsh.model.mesh.getNodes()
+    p = reshapoe(p, (:, 3))
+    display(size(p))
+    _, _, t = gmsh.model.mesh.getElements(3)
+    display(size(t))
     gmsh.write("bowl3D.msh")
     gmsh.finalize()
 end
