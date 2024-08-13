6c84fd74ef1dac4014fdf94f8e1dc7b3e1486d0c
hgpeterson <hgpeterson@caltech.edu>
2024-08-13 16:37:11 -0700

    3D inversion testing with restart=true

--- nonhydro3Dinv.jl
+++ nonhydro3Dinv.jl
@@ -1,22 +1,54 @@
 using NonhydroPG
-using Gridap
-using GridapGmsh
-using Gmsh: gmsh
-using IncompleteLU
-using Krylov
-using LinearAlgebra
-import LinearAlgebra: ldiv!, mul!
+using Gridap, GridapGmsh
+using CUDA, CUDA.CUSPARSE, CUDA.CUSOLVER
+using Krylov, SparseArrays, LinearAlgebra
+using HDF5, Printf
+using PyPlot
+
+pygui(false)
+plt.style.use("plots.mplstyle")
+plt.close("all")
+
+out_folder = "out"
+
+if !isdir(out_folder)
+    mkdir(out_folder)
+    mkdir("$out_folder/images")
+    mkdir("$out_folder/data")
+end
+
+function save(ux, uy, uz, p, b)
+    # fname = "$out_folder/data/nonhydro3D.vtu"
+    # writevtk(Ω, fname, cellfields=["u"=>ux, "v"=>uy, "w"=>uz, "p"=>p, "b"=>b])
+    # println(fname)
+    fname = "$out_folder/data/nonhydro3D.h5"
+    h5open(fname, "w") do file
+        write(file, "ux", ux.free_values)
+        write(file, "uy", uy.free_values)
+        write(file, "uz", uz.free_values)
+        write(file, "p", Vector(p.free_values))
+    end
+    println(fname)
+end
 
 # model
-model = GmshDiscreteModel("bowl3D_0.05.msh")
-# writevtk(model, "model")
+hres = 0.02
+model = GmshDiscreteModel(@sprintf("meshes/bowl3D_%0.2f.msh", hres))
+
+# mesh res
+pts, conns = get_p_t(model)
+h1 = [norm(pts[conns[i, 1], :] - pts[conns[i, 2], :]) for i ∈ axes(conns, 1)]
+h2 = [norm(pts[conns[i, 2], :] - pts[conns[i, 3], :]) for i ∈ axes(conns, 1)]
+h3 = [norm(pts[conns[i, 3], :] - pts[conns[i, 4], :]) for i ∈ axes(conns, 1)]
+h4 = [norm(pts[conns[i, 4], :] - pts[conns[i, 1], :]) for i ∈ axes(conns, 1)]
+hmin = minimum([h1; h2; h3; h4])
+hmax = maximum([h1; h2; h3; h4])
 
 # reference FE 
-order = 2
-reffe_ux = ReferenceFE(lagrangian, Float64, order;   space=:P)
-reffe_uy = ReferenceFE(lagrangian, Float64, order;   space=:P)
-reffe_uz = ReferenceFE(lagrangian, Float64, order;   space=:P)
-reffe_p  = ReferenceFE(lagrangian, Float64, order-1; space=:P)
+reffe_ux = ReferenceFE(lagrangian, Float64, 2)
+reffe_uy = ReferenceFE(lagrangian, Float64, 2)
+reffe_uz = ReferenceFE(lagrangian, Float64, 2)
+reffe_p  = ReferenceFE(lagrangian, Float64, 1)
 
 # test FESpaces
 Vx = TestFESpace(model, reffe_ux, conformity=:H1, dirichlet_tags=["bot"])
@@ -37,7 +69,7 @@ nz = Uz.space.nfree
 nu = nx + ny + nz
 np = P.space.space.nfree
 N = nu + np - 1
-println("N = ", N, " (", nu, " + ", np-1, ")")
+@printf("\nN = %d (%d + %d) ∼ 10^%d DOF\n", N, nu, np-1, floor(log10(N)))
 
 # initialize vectors
 ux = interpolate_everywhere(0, Ux)
@@ -46,109 +78,109 @@ uz = interpolate_everywhere(0, Uz)
 p  = interpolate_everywhere(0, P)
 
 # triangulation and integration measure
-degree = order^2
 Ω = Triangulation(model)
-dΩ = Measure(Ω, degree)
+dΩ = Measure(Ω, 4)
 
 # gradients 
 ∂x(u) = VectorValue(1.0, 0.0, 0.0)⋅∇(u)
 ∂y(u) = VectorValue(0.0, 1.0, 0.0)⋅∇(u)
 ∂z(u) = VectorValue(0.0, 0.0, 1.0)⋅∇(u)
 
+# depth
+H(x) = 1 - x[1]^2 - x[2]^2
+
 # forcing
-H(x) = sqrt(2 - x[1]^2 - x[2]^2) - 1
 ν(x) = 1
 
 # params
-ε² = 1
-println("δ = ", √(2ε²))
-pts, conns = get_p_t(model)
-h1 = [norm(pts[conns[i, 1], :] - pts[conns[i, 2], :]) for i ∈ axes(conns, 1)]
-h2 = [norm(pts[conns[i, 2], :] - pts[conns[i, 3], :]) for i ∈ axes(conns, 1)]
-h3 = [norm(pts[conns[i, 3], :] - pts[conns[i, 4], :]) for i ∈ axes(conns, 1)]
-h4 = [norm(pts[conns[i, 4], :] - pts[conns[i, 1], :]) for i ∈ axes(conns, 1)]
-hmin = minimum([h1; h2; h3; h4])
-hmax = maximum([h1; h2; h3; h4])
-println("hmin = ", hmin)
-println("hmax = ", hmax)
+ε² = 1e-2
 γ = 1
-f(x) = 1
+f₀ = 1
+β = 0
+f(x) = f₀ + β*x[2]
+println("\n---")
+println("Parameters:\n")
+@printf("ε² = %.1e (δ = %.1e, %.1e ≤ h ≤ %.1e)\n", ε², √(2ε²), hmin, hmax)
+@printf("f₀ = %.1e\n", f₀)
+@printf(" β = %.1e\n", β)
+@printf(" γ = %.1e\n", γ)
+println("---\n")
 
-# LHS matrix
-a((ux, uy, uz, p), (vx, vy, vz, q)) = 
-    # ∫( γ*ε²*∂x(ux)*∂x(vx)*ν +   γ*ε²*∂y(ux)*∂y(vx)*ν +   ε²*∂z(ux)*∂z(vx)*ν - uy*vx*f + ∂x(p)*vx +
-    #    γ*ε²*∂x(uy)*∂x(vy)*ν +   γ*ε²*∂y(uy)*∂y(vy)*ν +   ε²*∂z(uy)*∂z(vy)*ν + ux*vy*f + ∂y(p)*vy +
-    #  γ^2*ε²*∂x(uz)*∂x(vz)*ν + γ^2*ε²*∂y(uz)*∂y(vz)*ν + γ*ε²*∂z(uz)*∂z(vz)*ν +           ∂z(p)*vz +
-    #                                                               ∂x(ux)*q + ∂y(uy)*q + ∂z(uz)*q )dΩ
-    ∫( γ*ε²*∂x(ux)*∂x(vx)*ν +   γ*ε²*∂y(ux)*∂y(vx)*ν +   ε²*∂z(ux)*∂z(vx)*ν + ∂x(p)*vx +
-       γ*ε²*∂x(uy)*∂x(vy)*ν +   γ*ε²*∂y(uy)*∂y(vy)*ν +   ε²*∂z(uy)*∂z(vy)*ν + ∂y(p)*vy +
-     γ^2*ε²*∂x(uz)*∂x(vz)*ν + γ^2*ε²*∂y(uz)*∂y(vz)*ν + γ*ε²*∂z(uz)*∂z(vz)*ν + ∂z(p)*vz +
-                                                                  ∂x(ux)*q + ∂y(uy)*q + ∂z(uz)*q )dΩ
-# @time "assemble LHS" LHS = assemble_matrix(a, X, Y)
-@time "iLU" LHS_ilu = ilu(LHS, τ=1e-5)
+# filenames for LHS matrices
+LHS_inversion_fname = @sprintf("matrices/LHS_inversion_%e_%e_%e_%e_%e.h5", hres, ε², γ, f₀, β)
+
+# inversion LHS
+function assemble_LHS_inversion()
+    a_inversion((ux, uy, uz, p), (vx, vy, vz, q)) = 
+        ∫( γ*ε²*∂x(ux)*∂x(vx)*ν +   γ*ε²*∂y(ux)*∂y(vx)*ν +   ε²*∂z(ux)*∂z(vx)*ν - uy*vx*f + ∂x(p)*vx +
+           γ*ε²*∂x(uy)*∂x(vy)*ν +   γ*ε²*∂y(uy)*∂y(vy)*ν +   ε²*∂z(uy)*∂z(vy)*ν + ux*vy*f + ∂y(p)*vy +
+         γ^2*ε²*∂x(uz)*∂x(vz)*ν + γ^2*ε²*∂y(uz)*∂y(vz)*ν + γ*ε²*∂z(uz)*∂z(vz)*ν +           ∂z(p)*vz +
+                                                                      ∂x(ux)*q + ∂y(uy)*q + ∂z(uz)*q )dΩ
+    @time "assemble LHS_inversion" LHS_inversion = assemble_matrix(a_inversion, X, Y)
+    write_sparse_matrix(LHS_inversion_fname, LHS_inversion)
+    return LHS_inversion
+end
+
+if isfile(LHS_inversion_fname)
+    LHS_inversion = read_sparse_matrix(LHS_inversion_fname)
+else
+    LHS_inversion = assemble_LHS_inversion()
+end
+
+# Cuthill-McKee DOF reordering
+@time "RCM perm" begin 
+a_m(u, v) = ∫( u*v )dΩ
+M_ux = assemble_matrix(a_m, Ux, Vx)
+M_uy = assemble_matrix(a_m, Uy, Vy)
+M_uz = assemble_matrix(a_m, Uz, Vz)
+M_p  = assemble_matrix(a_m, P, Q)
+perm_ux = CUSOLVER.symrcm(M_ux) .+ 1
+perm_uy = CUSOLVER.symrcm(M_uy) .+ 1
+perm_uz = CUSOLVER.symrcm(M_uz) .+ 1
+perm_p  = CUSOLVER.symrcm(M_p)  .+ 1
+perm_inversion = [perm_ux; 
+                  perm_uy .+ nx; 
+                  perm_uz .+ nx .+ ny; 
+                  perm_p  .+ nx .+ ny .+ nz]
+inv_perm_inversion = invperm(perm_inversion)
+# plot_sparsity_pattern(LHS_inversion, fname="$out_folder/images/LHS_inversion.png")
+LHS_inversion = LHS_inversion[perm_inversion, perm_inversion]
+# plot_sparsity_pattern(LHS_inversion, fname="$out_folder/images/LHS_inversion_symrcm.png")
+end
+
+# put on GPU
+LHS_inversion = CuSparseMatrixCSR(LHS_inversion)
+CUDA.memory_status()
 
 # Krylov solver
-solver = GmresSolver(N, N, 20, Vector{eltype(LHS)})
+# solver_inversion = DqgmresSolver(N, N, 20, CuVector{Float64})
+solver_inversion = GmresSolver(N, N, 20, CuVector{Float64})
+solver_inversion.x .= 0
+tol = 1e-8
+itmax = 0
+# tol = 1e-7
+# itmax = 2000
+@printf("tol = %.1e, itmax = %d\n", tol, itmax)
 
-# initial guess
-b0(x) = x[3] + 0.10*exp(-(x[3] + H(x))/0.10)
-l0((vx, vy, vz, q)) = ∫( b0*vz )dΩ
-@time "assemble RHS 0" RHS0 = assemble_vector(l0, Y)
-# @time "solve 0" Krylov.solve!(solver, LHS, RHS0, M=LHS_ilu, verbose=1, ldiv=true)
-# ux.free_values .= solver.x[1:nx]
-# uy.free_values .= solver.x[nx+1:nx+ny]
-# uz.free_values .= solver.x[nx+ny+1:nx+ny+nz]
-# p = FEFunction(P, solver.x[nx+ny+nz+1:end])
-# writevtk(Ω, "out/nonhydro3D", cellfields=["u"=>ux, "v"=>uy, "w"=>uz, "p"=>p, "b"=>b0])
-
-# perturbation
-b1(x) = x[3] + 0.11*exp(-(x[3] + H(x))/0.11)
-l1((vx, vy, vz, q)) = ∫( b1*vz )dΩ
-@time "assemble RHS 1" RHS1 = assemble_vector(l1, Y)
-# @time "solve 1" Krylov.solve!(solver, LHS, RHS1, solver.x, M=LHS_ilu, verbose=1, ldiv=true)
-
-# assemble mass matrix for preconditioning
-a_m(p, q) = ∫( p*q )dΩ
-@time "assemble M" M = assemble_matrix(a_m, P, Q)
-@time "lu(M)" M_lu = lu(M/ε²)
-
-# set up block matrices: LHS = [A B; BT D] where D should be 0; RHS = [F; G]
-A = LHS[1:nu, 1:nu]
-B = LHS[nu+1:end, 1:nu]
-BT = LHS[1:nu, nu+1:end]
-
-# use incomplete LU for preconditioning on A
-@time "A_ilu" A_ilu = ilu(A, τ=1e-5)
-
-# P⁻¹ = [A⁻¹  0  ]
-#       [0    B⁻¹]
-struct DiagonalBlockPreconditioner{A,B}
-  A::A
-  B::B
+# inversion functions
+function invert!(solver_inversion, b)
+    l_inversion((vx, vy, vz, q)) = ∫( b*vz )dΩ
+    @time "build RHS_inversion" RHS_inversion = CuVector(assemble_vector(l_inversion, Y)[perm_inversion])
+    @time "invert!" Krylov.solve!(solver_inversion, LHS_inversion, RHS_inversion, solver_inversion.x, 
+                                  verbose=1, atol=tol, rtol=tol, itmax=itmax, restart=true)
+    return solver_inversion
 end
-function ldiv!(y, P::DiagonalBlockPreconditioner, x)
-  y[1:nu] = P.A\x[1:nu]
-  y[nu+1:end] = P.B\x[nu+1:end]
-  return y
+function update_u_p!(ux, uy, uz, p, solver_inversion)
+    sol = Vector(solver_inversion.x[inv_perm_inversion])
+    ux.free_values .= sol[1:nx]
+    uy.free_values .= sol[nx+1:nx+ny]
+    uz.free_values .= sol[nx+ny+1:nx+ny+nz]
+    p = FEFunction(P, sol[nx+ny+nz+1:end])
+    return ux, uy, uz, p
 end
-DBP = DiagonalBlockPreconditioner(A_ilu, M_lu)
-@time "solve with DBP" Krylov.solve!(solver, LHS, RHS0, M=DBP, ldiv=true, verbose=1)
 
-# P⁻¹ = [A⁻¹      0  ]
-#       [S⁻¹BA⁻¹  S⁻¹]
-struct LowerTriangularBlockPreconditioner{A,B,S}
-  A::A
-  B::B
-  S::S
-end
-function ldiv!(y, P::LowerTriangularBlockPreconditioner, x)
-  unew = P.A\x[1:nu]
-  y[1:nu] = unew
-  y[nu+1:end] = P.S\(P.B*(unew) - x[nu+1:end])
-  return y
-end
-LTBP = LowerTriangularBlockPreconditioner(A_ilu, B, M_lu)
-@time "solve with LTBP" Krylov.solve!(solver, LHS, RHS0, M=LTBP, ldiv=true, verbose=1)
-show(solver.stats)
-
-@time "solve with ILU" Krylov.solve!(solver, LHS, RHS0, M=LHS_ilu, verbose=1, ldiv=true)
+b(x) = x[3] + 0.1*exp(-(x[3] + H(x))/0.1)
+solver_inversion = invert!(solver_inversion, b)
+ux, uy, uz, p = update_u_p!(ux, uy, uz, p, solver_inversion)
+@time "profiles" plot_profiles(ux, uy, uz, b, 0.5, 0.0, H; t=0, fname="$out_folder/images/profiles.png")
+@time "save" save(ux, uy, uz, p, b)
