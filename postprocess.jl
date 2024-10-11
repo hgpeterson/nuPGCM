475ef62e248c3a36ff2767168df8e0c07a3eeb8e
hgpeterson <hgpeterson@caltech.edu>
2024-10-11 16:39:04 -0700

    profiles of terms in the momentum and buoyancy eqtns

--- postprocess.jl
+++ postprocess.jl
@@ -1,34 +1,34 @@
-# using NonhydroPG
-# using Gridap, GridapGmsh
-# using Printf
-# using PyPlot
+using NonhydroPG
+using Gridap, GridapGmsh
+using Printf
+using PyPlot
 
-# pygui(false)
-# plt.style.use("plots.mplstyle")
-# plt.close("all")
+pygui(false)
+plt.style.use("plots.mplstyle")
+plt.close("all")
 
-# # simulation folder
-# out_folder = "sim033"
+# simulation folder
+out_folder = "sim034"
 
-# # model
-# hres = 0.01
-# model = GmshDiscreteModel(@sprintf("meshes/bowl3D_%0.2f.msh", hres))
+# model
+hres = 0.01
+model = GmshDiscreteModel(@sprintf("meshes/bowl3D_%0.2f.msh", hres))
 
-# # # full grid
-# # m = Mesh(model)
+# # full grid
+# m = Mesh(model)
 
-# # # surface grid
-# # m_sfc = Mesh(model, "sfc")
+# # surface grid
+# m_sfc = Mesh(model, "sfc")
 
-# # FE spaces
-# X, Y, B, D = setup_FESpaces(model)
-# Ux, Uy, Uz, P = unpack_spaces(X)
+# FE spaces
+X, Y, B, D = setup_FESpaces(model)
+Ux, Uy, Uz, P = unpack_spaces(X)
 
-# # # triangulation
-# # Ω = Triangulation(model)
+# # triangulation
+# Ω = Triangulation(model)
 
-# # depth
-# H(x) = 1 - x[1]^2 - x[2]^2
+# depth
+H(x) = 1 - x[1]^2 - x[2]^2
 
 # # load state file
 # i_save = 10
@@ -71,7 +71,7 @@ function plot_animation()
     end
 end
 
-function compare(out_folders, labels, i_save)
+function profile_comparison(out_folders, labels, i_save)
     # plot name
     fname = @sprintf("profiles%03d.png", i_save)
 
@@ -143,5 +143,136 @@ function compare(out_folders, labels, i_save)
     plt.close()
 end
 
+∂x(u) = VectorValue(1, 0, 0)⋅∇(u)
+∂y(u) = VectorValue(0, 1, 0)⋅∇(u)
+∂z(u) = VectorValue(0, 0, 1)⋅∇(u)
+
+function momentum_and_buoyancy_balance(out_folder, i_save)
+    # parameters
+    ε² = 1e-4
+    f₀ = 1
+    β = 0
+    γ = 1/4
+    μϱ = 1
+    ν(x) = 1
+    κ(x) = 1e-2 + exp(-(x[3] + H(x))/0.1)
+
+    # profile
+    x = 0.5
+    y = 0.0
+    H0 = H([x, y])
+    z = range(-H0, 0, length=2^8)
+    points = [Point(x, y, zᵢ) for zᵢ ∈ z]
+
+    # load state file
+    statefile = @sprintf("%s/data/state%03d.h5", out_folder, i_save)
+    u, v, w, p, b, t = load_state(statefile)
+
+    # define functions
+    u = FEFunction(Ux, u)
+    v = FEFunction(Uy, v)
+    w = FEFunction(Uz, w)
+    p  = FEFunction(P, p)
+    b  = FEFunction(B, b)
+    px = ∂x(p)
+    py = ∂y(p)
+    pz = ∂z(p)
+    ux = ∂x(u)
+    vy = ∂y(v)
+    wz = ∂z(w)
+    bx = ∂x(b)
+    by = ∂y(b)
+    bz = ∂z(b)
+    # ∇²u = ε²*(γ*∂x(ν*∂x(u)) + γ*∂y(ν*∂y(u)) + ∂z(ν*∂z(u)))
+    # uz_fef = FEFunction(Ux, ∂z(u))
+    # ∇²u = ε²*∂z(uz_fef)
+
+    # caches
+    cache_u = Gridap.CellData.return_cache(u, points)
+    cache_v = Gridap.CellData.return_cache(v, points)
+    cache_w = Gridap.CellData.return_cache(w, points)
+    cache_px = Gridap.CellData.return_cache(px,  points)
+    cache_py = Gridap.CellData.return_cache(py,  points)
+    cache_pz = Gridap.CellData.return_cache(pz,  points)
+    cache_ux = Gridap.CellData.return_cache(ux,  points)
+    cache_vy = Gridap.CellData.return_cache(vy,  points)
+    cache_wz = Gridap.CellData.return_cache(wz,  points)
+    cache_b  = Gridap.CellData.return_cache(b,  points)
+    cache_bx = Gridap.CellData.return_cache(bx, points)
+    cache_by = Gridap.CellData.return_cache(by, points)
+    cache_bz = Gridap.CellData.return_cache(bz, points)
+    # cache_∇²u = Gridap.CellData.return_cache(∇²u, points)
+
+    # evals
+    us = nan_eval(cache_u, u, points)
+    vs = nan_eval(cache_v, v, points)
+    ws = nan_eval(cache_w, w, points)
+    pxs = nan_eval(cache_px, px, points)
+    pys = nan_eval(cache_py, py, points)
+    pzs = nan_eval(cache_pz, pz, points)
+    uxs = nan_eval(cache_ux, ux, points)
+    vys = nan_eval(cache_vy, vy, points)
+    wzs = nan_eval(cache_wz, wz, points)
+    bs  = nan_eval(cache_b,  b,  points)
+    bxs = nan_eval(cache_bx, bx, points)
+    bys = nan_eval(cache_by, by, points)
+    bzs = nan_eval(cache_bz, bz, points)
+    # ∇²us = nan_eval(cache_∇²u, ∇²u, points)
+
+    # plot name
+    fname = @sprintf("balances%03d.png", i_save)
+
+    # plot
+    fig, ax = plt.subplots(1, 5, figsize=(10, 3.2))
+    ax[1].set_ylabel(L"z")
+    ax[1].set_xlabel(L"$x$-momentum")
+    ax[2].set_xlabel(L"$y$-momentum")
+    ax[3].set_xlabel(L"$z$-momentum")
+    ax[4].set_xlabel("Continuity")
+    ax[5].set_xlabel("Buoyancy")
+    ax[2].set_yticklabels([])
+    ax[3].set_yticklabels([])
+    ax[4].set_yticklabels([])
+    ax[5].set_yticklabels([])
+    for a ∈ ax 
+        a.set_ylim(z[1], 0) 
+        a.ticklabel_format(axis="x", style="sci", scilimits=(-2,2))
+        a.spines["left"].set_visible(false)
+        a.axvline(0, color="k", linewidth=0.5, linestyle="-")
+    end
+    # x momentum
+    ax[1].plot(-(f₀ + β*y)*vs, z, label=L"-fv")
+    ax[1].plot(-pxs, z, label=L"-\partial_x p")
+    # ax[1].plot(∇²us, z, label=L"\varepsilon^2 \nabla \cdot (K_\nu \nabla u)")
+    ax[1].legend()
+    # y momentum
+    ax[2].plot((f₀ + β*y)*us, z, label=L"fu")
+    ax[2].plot(-pys, z, label=L"-\partial_y p")
+    ax[2].legend()
+    # z momentum
+    ax[3].plot(bs, z, label=L"b")
+    ax[3].plot(pzs, z, label=L"\partial_z p")
+    ax[3].legend()
+    # continuity
+    ax[4].plot(uxs, z, label=L"\partial_x u")
+    ax[4].plot(vys, z, label=L"\partial_y v")
+    ax[4].plot(wzs, z, label=L"\partial_z w")
+    ax[4].legend()
+    # buoyancy
+    ax[5].plot(us.*bxs, z, label=L"u \partial_x b")
+    ax[5].plot(vs.*bys, z, label=L"v \partial_y b")
+    ax[5].plot(ws.*(1 .+ bzs), z, label=L"w (1 + \partial_z b)")
+    ax[5].legend()
+    if t === nothing
+        ax[1].set_title(latexstring(@sprintf("x = %1.2f, \\quad y = %1.2f", x, y)))
+    else
+        ax[1].set_title(latexstring(@sprintf("x = %1.2f, \\quad y = %1.2f, \\quad t = %1.2f", x, y, t)))
+    end
+    savefig(fname)
+    println(fname)
+    plt.close()
+end
+
 # plot_animation()
-compare(["sim034", "sim033", "sim030"], [L"\beta = 0.0", L"\beta = 0.5", L"\beta = 1.0"], 4)
+# profile_comparison(["sim034", "sim033", "sim030"], [L"\beta = 0.0", L"\beta = 0.5", L"\beta = 1.0"], 4)
+momentum_and_buoyancy_balance("sim033", 4)
