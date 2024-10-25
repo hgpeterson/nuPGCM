51cb6a5d964ddd71a3bcaf0fcf9c0e6a0646bbd7
hgpeterson <hgpeterson@caltech.edu>
2024-10-25 13:37:49 -0700

    handle NaNs in plots and better label for time

--- scratch/postprocess.jl
+++ scratch/postprocess.jl
@@ -4,25 +4,19 @@ using Printf
 using PyPlot
 
 pygui(false)
-plt.style.use("plots.mplstyle")
+plt.style.use("../plots.mplstyle")
 plt.close("all")
 
 # simulation folder
-out_folder = "../sims/sim034"
+out_folder = "../sims/sim035"
 
-# model
-hres = 0.01
-model = GmshDiscreteModel(@sprintf("../meshes/bowl3D_%0.2f.msh", hres))
+# # model
+# hres = 0.01
+# model = GmshDiscreteModel(@sprintf("../meshes/bowl3D_%0.2f.msh", hres))
 
-# # full grid
-# m = Mesh(model)
-
-# # surface grid
-# m_sfc = Mesh(model, "sfc")
-
-# FE spaces
-X, Y, B, D = setup_FESpaces(model)
-Ux, Uy, Uz, P = unpack_spaces(X)
+# # FE spaces
+# X, Y, B, D = setup_FESpaces(model)
+# Ux, Uy, Uz, P = unpack_spaces(X)
 
 # # triangulation
 # Ω = Triangulation(model)
@@ -30,50 +24,49 @@ Ux, Uy, Uz, P = unpack_spaces(X)
 # depth
 H(x) = 1 - x[1]^2 - x[2]^2
 
-# # load state file
-# i_save = 10
-# statefile = @sprintf("%s/data/state%03d.h5", out_folder, i_save)
-# ux, uy, uz, p, b, t = load_state(statefile)
-# ux = FEFunction(Ux, ux)
-# uy = FEFunction(Uy, uy)
-# uz = FEFunction(Uz, uz)
-# p  = FEFunction(P, p)
-# b  = FEFunction(B, b)
+# load state file
+i_save = 3
+statefile = @sprintf("%s/data/state%03d.h5", out_folder, i_save)
+ux, uy, uz, p, b, t = load_state(statefile)
+ux = FEFunction(Ux, ux)
+uy = FEFunction(Uy, uy)
+uz = FEFunction(Uz, uz)
+p  = FEFunction(P, p)
+b  = FEFunction(B, b)
 
 # # save vtu
 # save_state_vtu(ux, uy, uz, p, b, Ω; fname=@sprintf("%s/data/state%03d.vtu", out_folder, i_save))
 
 # plot_slice(ux, b; x=0,    t=t, cb_label=L"Zonal flow $u$", fname=@sprintf("%s/images/u_xslice_%03d.png", out_folder, i_save))
-# plot_slice(ux, b; y=0,    t=t, cb_label=L"Zonal flow $u$", fname=@sprintf("%s/images/u_yslice_%03d.png", out_folder, i_save))
+plot_slice(ux, b; y=0,    t=t, cb_label=L"Zonal flow $u$", fname=@sprintf("%s/images/u_yslice_%03d.png", out_folder, i_save))
 # plot_slice(ux, b; z=-0.5, t=t, cb_label=L"Zonal flow $u$", fname=@sprintf("%s/images/u_zslice_%03d.png", out_folder, i_save))
 # plot_slice(uy, b; x=0,    t=t, cb_label=L"Meridional flow $v$", fname=@sprintf("%s/images/v_xslice_%03d.png", out_folder, i_save))
-# plot_slice(uy, b; y=0,    t=t, cb_label=L"Meridional flow $v$", fname=@sprintf("%s/images/v_yslice_%03d.png", out_folder, i_save))
+plot_slice(uy, b; y=0,    t=t, cb_label=L"Meridional flow $v$", fname=@sprintf("%s/images/v_yslice_%03d.png", out_folder, i_save))
 # plot_slice(uy, b; z=-0.5, t=t, cb_label=L"Meridional flow $v$", fname=@sprintf("%s/images/v_zslice_%03d.png", out_folder, i_save))
 # plot_slice(uz, b; x=0,    t=t, cb_label=L"Vertical flow $w$", fname=@sprintf("%s/images/w_xslice_%03d.png", out_folder, i_save))
 # plot_slice(uz, b; y=0,    t=t, cb_label=L"Vertical flow $w$", fname=@sprintf("%s/images/w_yslice_%03d.png", out_folder, i_save))
 # plot_slice(uz, b; z=-0.5, t=t, cb_label=L"Vertical flow $w$", fname=@sprintf("%s/images/w_zslice_%03d.png", out_folder, i_save))
-# plot_slice(ux, uy, b; z=0.0, t=t, cb_label=L"Horizontal speed $\sqrt{u^2 + v^2}$", fname=@sprintf("%s/images/uv_zslice_%03d.png", out_folder, i_save))
+plot_slice(ux, uy, b; z=0.0, t=t, cb_label=L"Horizontal speed $\sqrt{u^2 + v^2}$", fname=@sprintf("%s/images/uv_zslice_%03d.png", out_folder, i_save))
 # plot_slice(ux, uz, b; y=0.0, t=t, cb_label=L"Speed $\sqrt{u^2 + w^2}$", fname=@sprintf("%s/images/uw_yslice_%03d.png", out_folder, i_save))
 # println()
 
 function plot_animation()
-    for i_save ∈ 16:18
+    for i_save ∈ [50]
         statefile = @sprintf("%s/data/state%03d.h5", out_folder, i_save)
         ux, uy, uz, p, b, t = load_state(statefile)
         ux = FEFunction(Ux, ux)
         uy = FEFunction(Uy, uy)
         # uz = FEFunction(Uz, uz)
         b  = FEFunction(B, b)
-        # plot_slice(ux, uz, b; y=0.0, t=t, cb_label=L"Speed $\sqrt{u^2 + w^2}$", cb_max=6e-2, fname=@sprintf("%s/images/uw_yslice_%03d.png", out_folder, i_save))
-        # plot_slice(uy, b; y=0.0, t=t, cb_label=L"Meridional flow $v$", cb_max=1e-1, fname=@sprintf("%s/images/v_yslice_%03d.png", out_folder, i_save))
-        # plot_slice(uz, b; z=-0.5, t=t, cb_label=L"Vertical flow $w$", cb_max=4e-3, fname=@sprintf("%s/images/w_zslice_%03d.png", out_folder, i_save))
-        plot_slice(ux, uy, b; z=0.0, t=t, cb_label=L"Horizontal speed $\sqrt{u^2 + v^2}$", fname=@sprintf("%s/images/u_sfc_%03d.png", out_folder, i_save))
+        plot_slice(ux, b;     y=0, t=t, cb_label=L"Zonal flow $u$",                      cb_max=6e-3,   fname=@sprintf("%s/images/u_yslice_%03d.png", out_folder, i_save))
+        plot_slice(uy, b;     y=0, t=t, cb_label=L"Meridional flow $v$",                 cb_max=1.5e-1, fname=@sprintf("%s/images/v_yslice_%03d.png", out_folder, i_save))
+        plot_slice(ux, uy, b; z=0, t=t, cb_label=L"Horizontal speed $\sqrt{u^2 + v^2}$", cb_max=2e-1,   fname=@sprintf("%s/images/u_sfc_%03d.png", out_folder, i_save))
     end
 end
 
 function profile_comparison(out_folders, labels, i_save)
     # plot name
-    fname = @sprintf("profiles%03d.png", i_save)
+    fname = @sprintf("%s/profiles%03d.png", out_folder, i_save)
 
     # setup points
     x = 0.5
@@ -86,24 +79,22 @@ function profile_comparison(out_folders, labels, i_save)
     t = 0.
 
     # plot
-    fig, ax = plt.subplots(1, 4, figsize=(8, 3.2))
+    fig, ax = plt.subplots(1, 3, figsize=(6, 3.2))
     ax[1].set_ylabel(L"z")
     ax[1].set_xlabel(L"u")
     ax[2].set_xlabel(L"v")
-    ax[3].set_xlabel(L"w")
-    ax[4].set_xlabel(L"\partial_z b")
+    ax[3].set_xlabel(L"\partial_z b")
     ax[2].set_yticklabels([])
     ax[3].set_yticklabels([])
-    ax[4].set_yticklabels([])
     for a ∈ ax 
         a.set_ylim(z[1], 0) 
         a.ticklabel_format(axis="x", style="sci", scilimits=(-2,2))
     end
-    for a ∈ ax[1:3]
+    for a ∈ ax[1:2]
         a.spines["left"].set_visible(false)
         a.axvline(0, color="k", linewidth=0.5, linestyle="-")
     end
-    ax[4].set_xlim(0, 1.1)
+    ax[3].set_xlim(0, 1.1)
     for i ∈ eachindex(out_folders)
         out_folder = out_folders[i]
         statefile = @sprintf("../sims/%s/data/state%03d.h5", out_folder, i_save)
@@ -126,17 +117,16 @@ function profile_comparison(out_folders, labels, i_save)
         bzs .+= 1
         uxs[1] = 0
         uys[1] = 0
-        uzs[1] = 0
-        ax[1].plot(uxs, z, label=labels[i])
-        ax[2].plot(uys, z)
-        ax[3].plot(uzs, z)
-        ax[4].plot(bzs, z)
+        ax[1].plot(uxs[isnan.(uxs) .== 0], z[isnan.(uxs) .== 0], label=labels[i])
+        ax[2].plot(uys[isnan.(uys) .== 0], z[isnan.(uys) .== 0])
+        ax[3].plot(bzs[isnan.(bzs) .== 0], z[isnan.(bzs) .== 0])
     end
     ax[1].legend()
     if t === nothing
-        ax[1].set_title(latexstring(@sprintf("x = %1.2f, \\quad y = %1.2f", x, y)))
+        ax[2].set_title(latexstring(@sprintf("x = %1.2f, \\quad y = %1.2f", x, y)))
     else
-        ax[1].set_title(latexstring(@sprintf("x = %1.2f, \\quad y = %1.2f, \\quad t = %1.2f", x, y, t)))
+        log10t = floor(log10(t))
+        ax[2].set_title(latexstring(@sprintf("x = %1.1f, \\quad y = %1.1f, \\quad t = %1.1f \\times 10^{%d} \\varepsilon^2/\\mu\\varrho", x, y, t/10^log10t, log10t)))
     end
     savefig(fname)
     println(fname)
@@ -266,7 +256,7 @@ function momentum_and_buoyancy_balance(out_folder, i_save)
     if t === nothing
         ax[1].set_title(latexstring(@sprintf("x = %1.2f, \\quad y = %1.2f", x, y)))
     else
-        ax[1].set_title(latexstring(@sprintf("x = %1.2f, \\quad y = %1.2f, \\quad t = %1.2f", x, y, t)))
+        ax[1].set_title(latexstring(@sprintf("x = %1.1f, \\quad y = %d, \\quad t = %1.2f", x, y, t)))
     end
     savefig(fname)
     println(fname)
@@ -274,5 +264,6 @@ function momentum_and_buoyancy_balance(out_folder, i_save)
 end
 
 # plot_animation()
-# profile_comparison(["sim034", "sim033", "sim030"], [L"\beta = 0.0", L"\beta = 0.5", L"\beta = 1.0"], 4)
-momentum_and_buoyancy_balance("sim033", 4)
+# profile_comparison(["sim038", "sim033", "sim030"], [L"\beta = 0.0", L"\beta = 0.5", L"\beta = 1.0"], 3)
+# profile_comparison(["sim035", "sim036", "sim037"], [L"\beta = 0.0", L"\beta = 0.5", L"\beta = 1.0"], 3)
+# momentum_and_buoyancy_balance("sim033", 4)
