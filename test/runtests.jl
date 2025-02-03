36666f1234b25045d24ce16a8a75ee87e52bc543
hgpeterson <hgpeterson@caltech.edu>
2025-02-03 14:46:44 -0800

    E2E inversion tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,13 @@ using Test
 
 @testset "nuPGCM Tests" begin
 
-    @testset "Utils tests" begin
+    @testset "Unit tests" begin
         include("utils_tests.jl")
+    end
+
+    @testset "End-to-end tests" begin
+        include("inversion_tests.jl")
+        include("evolution_tests.jl")
     end
 
 end
