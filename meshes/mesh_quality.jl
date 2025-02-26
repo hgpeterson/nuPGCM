653c057f7ac9c8d525a941563ff307aff9896bb0
hgpeterson <hgpeterson@caltech.edu>
2025-02-26 09:24:56 -0800

    Add volume statistics

--- meshes/mesh_quality.jl
+++ meshes/mesh_quality.jl
@@ -1,7 +1,13 @@
-using LinearAlgebra, Statistics, HDF5, Printf
-using Gridap, GridapGmsh, nuPGCM
+using nuPGCM
+using LinearAlgebra
+using Statistics
+using HDF5
+using JLD2
+using Gridap
+using GridapGmsh
 using Gmsh: gmsh
 using PyPlot
+using Printf
 
 pygui(false)
 plt.style.use("../plots.mplstyle")
@@ -71,53 +77,104 @@ function inner_angles(p, t)
     return sort(θ[:])
 end
 
-function print_stats(title, θ)
+
+"""
+    v = volume(p1, p2, p3, p4)
+
+Compute the volume of a tetrahedron defined by the vertices `p1`, `p2`, `p3`,
+and `p4`.
+"""
+function volume(p1, p2, p3, p4)
+    return abs(det(hcat(p1 - p4, p2 - p4, p3 - p4)))/6
+end
+
+"""
+    v = volumes(p, t)
+
+Compute (and sort) the volumes of a tetrahedral mesh defined by the vertices 
+`p` and connectivities `t`.
+"""
+function volumes(p, t)
+    v = zeros(size(t, 1))
+    for k ∈ axes(t, 1)
+        pts = (p[t[k, i], :] for i ∈ 1:4)
+        v[k] = volume(pts...)
+    end
+    return sort(v)
+end
+
+function print_stats(title, θ; variable_name="θ")
     @info begin
     msg = title
-    msg *= @sprintf("\n%f ≤ θ ≤ %f\n", minimum(θ), maximum(θ))
-    msg *= @sprintf("mean(θ):   %f\n", mean(θ))
-    msg *= @sprintf("median(θ): %f\n", median(θ))
-    msg *= @sprintf("std(θ):    %f\n", std(θ))
+    msg *= @sprintf("\n%e ≤ %s ≤ %e\n", minimum(θ), variable_name, maximum(θ))
+    msg *= @sprintf("mean(%s):   %e\n", variable_name, mean(θ))
+    msg *= @sprintf("median(%s): %e\n", variable_name, median(θ))
+    msg *= @sprintf("std(%s):    %e\n", variable_name, std(θ))
     msg
     end
 end
 
 # resolution
-h = 0.02
+h = 0.05
 
 # # distmesh
 # fname = @sprintf("bowl3D_%0.2fdm.h5", h)
 # p = h5read(fname, "p")
 # t = h5read(fname, "t")
+# close(fname)
 # @time θ_dm = inner_angles(p, t)
 # println("DistMesh:")
 # print_stats(θ_dm)
 
-# gmsh
-fname = @sprintf("bowl3D_%0.2f.msh", h)
-p, t = get_p_t(fname)
-@time θ_gm = inner_angles(p, t)
-print_stats("Gmsh", θ_gm)
+# # gmsh
+# fname = @sprintf("bowl3D_%0.2f.msh", h)
+# p, t = get_p_t(fname)
+# # @time θ_gm = inner_angles(p, t)
+# # print_stats("Gmsh", θ_gm)
+# @time v_gm = volumes(p, t)
+# print_stats("Gmsh", v_gm, variable_name="v")
 
 # jörn
-fname = @sprintf("bowl3D_%0.2fjc.h5", h)
-p = h5read(fname, "p")
-t = h5read(fname, "t")
-@time θ_jc = inner_angles(p, t)
-print_stats("Jörn", θ_jc)
+# fname = @sprintf("bowl3D_%0.2fjc.h5", h)
+# p = h5read(fname, "p")
+# t = h5read(fname, "t")
+# close(fname)
+d = jldopen("mesh_010_v2.jld2", "r")
+p = d["p"]
+t = d["t"]
+close(d)
+# @time θ_jc = inner_angles(p, t)
+# print_stats("Jörn", θ_jc)
+@time v_jc = volumes(p, t)
+print_stats("Jörn", v_jc, variable_name="v")
 
-# plot
-fig, ax = plt.subplots(1)
-# ax.hist(θ_dm, bins=100, density=true, alpha=0.5, label="DistMesh")
-ax.hist(θ_gm, bins=100, density=true, alpha=0.5, label="Gmsh")
-ax.hist(θ_jc, bins=100, density=true, alpha=0.5, label="Jörn")
-ax.legend()
-ax.set_xlabel("Inner angle (degrees)")
-ax.set_ylabel("Density")
-ax.set_xlim(0, 120)
-ax.set_xticks(0:30:120)
-ax.set_ylim(0, 0.05)
-fname = @sprintf("../out/inner_angles_%.2f.png", h)
-savefig(fname)
-@info "Saved '$fname'"
-plt.close()
+# # plot
+# fig, ax = plt.subplots(1)
+# # ax.hist(θ_dm, bins=100, density=true, alpha=0.5, label="DistMesh")
+# ax.hist(θ_gm, bins=100, density=true, alpha=0.5, label="Gmsh")
+# ax.hist(θ_jc, bins=100, density=true, alpha=0.5, label="Jörn")
+# ax.legend()
+# ax.set_xlabel("Inner angle (degrees)")
+# ax.set_ylabel("Density")
+# ax.set_xlim(0, 120)
+# ax.set_xticks(0:30:120)
+# ax.set_ylim(0, 0.05)
+# fname = @sprintf("../out/inner_angles_%.2f.png", h)
+# savefig(fname)
+# @info "Saved '$fname'"
+# plt.close()
+
+# # plot
+# fig, ax = plt.subplots(1)
+# ax.hist(v_gm, bins=100, density=true, alpha=0.5, label="Gmsh")
+# # ax.hist(v_jc, bins=100, density=true, alpha=0.5, label="Jörn")
+# ax.legend()
+# ax.set_xlabel("Volume")
+# ax.set_ylabel("Density")
+# # ax.set_xlim(0, 120)
+# # ax.set_xticks(0:30:120)
+# # ax.set_ylim(0, 0.05)
+# fname = @sprintf("../out/volumes_%.2f.png", h)
+# savefig(fname)
+# @info "Saved '$fname'"
+# plt.close()
