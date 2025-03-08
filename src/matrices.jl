d96376bbc80c1ec9ccf17fe7c66a397e97c1e6e4
hgpeterson <hgpeterson@caltech.edu>
2025-03-07 16:56:55 -0800

    Test isotropic mesh

--- src/matrices.jl
+++ src/matrices.jl
@@ -27,16 +27,16 @@ function build_A_inversion(mesh::Mesh, α, ε, ν, f; fname=nothing)
     α⁴ε² = α^4*ε^2
 
     # bilinear form
-    a((ux, uy, uz, p), (vx, vy, vz, q)) =
-        ∫( α²ε²*∂x(ux)*∂x(vx)*ν + α²ε²*∂y(ux)*∂y(vx)*ν +   ε²*∂z(ux)*∂z(vx)*ν - uy*vx*f + ∂x(p)*vx +
-           α²ε²*∂x(uy)*∂x(vy)*ν + α²ε²*∂y(uy)*∂y(vy)*ν +   ε²*∂z(uy)*∂z(vy)*ν + ux*vy*f + ∂y(p)*vy +
-           α⁴ε²*∂x(uz)*∂x(vz)*ν + α⁴ε²*∂y(uz)*∂y(vz)*ν + α²ε²*∂z(uz)*∂z(vz)*ν +           ∂z(p)*vz +
-                                                                     ∂x(ux)*q + ∂y(uy)*q + ∂z(uz)*q )dΩ
     # a((ux, uy, uz, p), (vx, vy, vz, q)) =
-    #     ∫(α²ε²*∂x(ux)*∂x(vx)*ν + α²ε²*∂y(ux)*∂y(vx)*ν + α²ε²*∂z(ux)*∂z(vx)*ν - uy*vx*f + ∂x(p)*vx +
-    #       α²ε²*∂x(uy)*∂x(vy)*ν + α²ε²*∂y(uy)*∂y(vy)*ν + α²ε²*∂z(uy)*∂z(vy)*ν + ux*vy*f + ∂y(p)*vy +
-    #       α²ε²*∂x(uz)*∂x(vz)*ν + α²ε²*∂y(uz)*∂y(vz)*ν + α²ε²*∂z(uz)*∂z(vz)*ν +           ∂z(p)*vz +
-    #                                                                 ∂x(ux)*q + ∂y(uy)*q + ∂z(uz)*q )dΩ
+    #     ∫( α²ε²*∂x(ux)*∂x(vx)*ν + α²ε²*∂y(ux)*∂y(vx)*ν +   ε²*∂z(ux)*∂z(vx)*ν - uy*vx*f + ∂x(p)*vx +
+    #        α²ε²*∂x(uy)*∂x(vy)*ν + α²ε²*∂y(uy)*∂y(vy)*ν +   ε²*∂z(uy)*∂z(vy)*ν + ux*vy*f + ∂y(p)*vy +
+    #        α⁴ε²*∂x(uz)*∂x(vz)*ν + α⁴ε²*∂y(uz)*∂y(vz)*ν + α²ε²*∂z(uz)*∂z(vz)*ν +           ∂z(p)*vz +
+    #                                                                  ∂x(ux)*q + ∂y(uy)*q + ∂z(uz)*q )dΩ
+    a((ux, uy, uz, p), (vx, vy, vz, q)) =
+        ∫(α²ε²*∂x(ux)*∂x(vx)*ν + α²ε²*∂y(ux)*∂y(vx)*ν + α²ε²*∂z(ux)*∂z(vx)*ν - uy*vx*f + ∂x(p)*vx +
+          α²ε²*∂x(uy)*∂x(vy)*ν + α²ε²*∂y(uy)*∂y(vy)*ν + α²ε²*∂z(uy)*∂z(vy)*ν + ux*vy*f + ∂y(p)*vy +
+          α²ε²*∂x(uz)*∂x(vz)*ν + α²ε²*∂y(uz)*∂y(vz)*ν + α²ε²*∂z(uz)*∂z(vz)*ν +           ∂z(p)*vz +
+                                                                    ∂x(ux)*q + ∂y(uy)*q + ∂z(uz)*q )dΩ
 
     # assemble 
     @time "build A_inversion" A = assemble_matrix(a, X_trial, X_test)
@@ -55,18 +55,36 @@ end
 
 Assemble the RHS matrix for the inversion problem.
 """
-# function build_B_inversion(mesh::Mesh, α)
 function build_B_inversion(mesh::Mesh)
     # unpack
     X_test, B_trial, dΩ = mesh.spaces.X_test, mesh.spaces.B_trial, mesh.dΩ
     W_test = X_test[3]
 
-    # coefficient
-    # α⁻¹ = 1/α
-
     # bilinear form
     a(b, vz) = ∫( b*vz )dΩ
-    # a(b, vz) = ∫( α⁻¹*b*vz )dΩ
+
+    # assemble
+    @time "B_inversion" B = assemble_matrix(a, B_trial, W_test)
+
+    # convert to N × nb matrix
+    nu, nv, nw, np, nb = get_n_dofs(mesh.dofs)
+    N = nu + nv + nw + np
+    I, J, V = findnz(B)
+    I .+= nu + nv
+    B = sparse(I, J, V, N, nb)
+
+    return B
+end
+function build_B_inversion(mesh::Mesh, α)
+    # unpack
+    X_test, B_trial, dΩ = mesh.spaces.X_test, mesh.spaces.B_trial, mesh.dΩ
+    W_test = X_test[3]
+
+    # coefficient
+    α⁻¹ = 1/α
+
+    # bilinear form
+    a(b, vz) = ∫( α⁻¹*b*vz )dΩ
 
     # assemble
     @time "B_inversion" B = assemble_matrix(a, B_trial, W_test)
