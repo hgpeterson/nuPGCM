41e9baaaf6d8ccd142256d157550594dd333fa14
hgpeterson <hgpeterson@caltech.edu>
2025-04-22 15:59:49 -0700

    Add `\alpha` to `\theta` definition

--- scratch/run.jl
+++ scratch/run.jl
@@ -14,15 +14,15 @@ ENV["JULIA_DEBUG"] = nuPGCM
 set_out_dir!(".")
 
 # architecture and dimension
-arch = GPU()
+arch = CPU()
 dim = 2
 
 # params/funcs
-ε = 1e-1
+ε = 2e-2
 α = 1/2
 μϱ = 1e0
 N² = 1e0/α
-Δt = 1e-4*μϱ/ε^2
+Δt = 1e-5*μϱ/ε^2/α^2
 params = Parameters(ε, α, μϱ, N², Δt)
 f₀ = 1.0
 β = 0.0
@@ -30,14 +30,13 @@ f(x) = f₀ + β*x[2]
 H(x) = α*(1 - x[1]^2 - x[2]^2)
 ν(x) = 1
 κ(x) = 1e-2 + exp(-(x[3] + H(x))/(0.1*α))
-T = 5e-2*μϱ/ε^2
-force_build_inversion_matrices = true
-force_build_evolution_matrices = true
+T = 100*Δt
+force_build_inversion_matrices = false
+force_build_evolution_matrices = false
 
 # mesh
-h = 8e-3
+h = 7e-3
 mesh = Mesh(@sprintf("../meshes/bowl%sD_%e_%e.msh", dim, h, α))
-# mesh = Mesh(@sprintf("../meshes/bowl%sD_%e_%e_squash%d.msh", dim, h, 1, 1/α))
 
 # build inversion matrices
 A_inversion_fname = @sprintf("../matrices/A_inversion_%sD_%e_%e_%e_%e_%e.jld2", dim, h, ε, α, f₀, β)
@@ -53,8 +52,6 @@ else
     close(file)
     B_inversion = nuPGCM.build_B_inversion(mesh, params)
 end
-@info "Approximate condition number of A_inversion: " cond(A_inversion, 1)
-error()
 
 # re-order dofs
 A_inversion = A_inversion[mesh.dofs.p_inversion, mesh.dofs.p_inversion]
@@ -81,7 +78,7 @@ inversion_toolkit = InversionToolkit(A_inversion, P_inversion, B_inversion)
 # save_state(model, "$out_dir/data/state.jld2")
 
 # build evolution matrices and test against saved matrices
-θ = Δt/2 * ε^2 / μϱ
+θ = Δt/2 * α^2 * ε^2 / μϱ 
 A_diff_fname = @sprintf("../matrices/A_diff_%sD_%e_%e_%e.jld2", dim, h, θ, α)
 A_adv_fname = @sprintf("../matrices/A_adv_%sD_%e_%e.jld2", dim, h, α)
 if !isfile(A_diff_fname) || !isfile(A_adv_fname) || force_build_evolution_matrices
@@ -126,6 +123,6 @@ evolution_toolkit = EvolutionToolkit(A_adv, P_adv, A_diff, P_diff, B_diff, b_dif
 model = rest_state_model(arch, params, mesh, inversion_toolkit, evolution_toolkit)
 
 # solve
-run!(model, T; t_plot=T÷5)
+run!(model, T; t_plot=T, t_save=T)
 
 println("Done.")
