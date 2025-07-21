a437022f8131b8247c70cbd160c87dd603b996f1
hgpeterson <hgpeterson@caltech.edu>
2025-07-21 10:13:32 -0700

    Add `save_vtk`

--- src/IO.jl
+++ src/IO.jl
@@ -5,15 +5,30 @@ function save_state(model::Model, ofile)
     @info "Model state saved to '$ofile'"
 end
 
-function set_state_from_file!(model::Model, ifile)
+function set_state_from_file!(s::State, ifile)
     d = jldopen(ifile, "r")
-    model.state.u.free_values .= d["u"]
-    model.state.v.free_values .= d["v"]
-    model.state.w.free_values .= d["w"]
-    model.state.p.free_values.args[1] .= d["p"]
-    model.state.b.free_values .= d["b"]
-    model.state.t = d["t"]
+    s.u.free_values .= d["u"]
+    s.v.free_values .= d["v"]
+    s.w.free_values .= d["w"]
+    s.p.free_values.args[1] .= d["p"]
+    s.b.free_values .= d["b"]
+    s.t = d["t"]
     close(d)
-    @info "Model state set from '$ifile'"
-    return model
+    @info "State set from '$ifile'"
+    return s
+end
+
+function save_vtk(s::State, m::Mesh; ofile="state.vtu")
+    writevtk(m.Ω, ofile, cellfields=[
+        "u" => s.u, 
+        "v" => s.v, 
+        "w" => s.w, 
+        "p" => s.p, 
+        "b" => s.b
+    ])
+
+    @info "VTK state saved to '$ofile'"
+end
+function save_vtk(m::Model; ofile="state.vtu")
+    save_vtk(m.state, m.mesh; ofile)
 end
