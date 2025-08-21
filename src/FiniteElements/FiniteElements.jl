a172dfb05012a786b6614aa11129c99bca8fe899
Henry Peterson <hgpeterson@caltech.edu>
2025-08-21 12:57:34 -0700

    Add VTU saving

--- src/FiniteElements/FiniteElements.jl
+++ src/FiniteElements/FiniteElements.jl
@@ -2,6 +2,8 @@ module FiniteElements
 
 using Gmsh: gmsh
 using LinearAlgebra
+using SparseArrays
+using WriteVTK
 using ProgressMeter
 
 export 
@@ -18,10 +20,21 @@ export
     Jacobians,
     QuadratureRule,
     integrate,
-    ∫
+    ∫,
+
+    # shape_functions.jl
+    Lagrange,
+    φ,
+    ∇φ,
+
+    # vtk.jl
+    save_vtu
 
 include("elements.jl")
 include("mesh.jl")
 include("quadrature.jl")
+include("shape_functions.jl")
+include("matrix_assembly.jl")
+include("vtu.jl")
 
 end # module
---
+++ src/FiniteElements/vtu.jl
@@ -0,0 +1,14 @@
+function save_vtu(mesh::Mesh, filename, data)
+    points = Matrix(mesh.nodes')
+    cell_type = get_vtk_cell_type(mesh.element_type)
+    cells = [MeshCell(cell_type, mesh.elements[k, :]) for k in axes(mesh.elements, 1)]
+    vtk_grid(filename, points, cells, append=false) do vtk
+        for (name, values) in data
+            vtk[name] = values
+        end
+    end
+    @info "VTU file saved to $filename"
+end
+
+get_vtk_cell_type(::Triangle) = VTKCellTypes.VTK_TRIANGLE
+get_vtk_cell_type(::Tetrahedron) = VTKCellTypes.VTK_TETRA
