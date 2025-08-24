62fb4d2ce9e7fd9a1b631ebbf6e79c637c81d0c6
hgpeterson <hgpeterson@caltech.edu>
2025-08-23 17:01:21 -0700

    Update example

--- examples/run.jl
+++ examples/run.jl
@@ -2,56 +2,67 @@ using nuPGCM
 using JLD2
 using LinearAlgebra
 using Printf
-using PyPlot
 
-pygui(false)
-plt.style.use("../plots.mplstyle")
-plt.close("all")
+set_out_dir!(joinpath(@__DIR__, ""))
 
-set_out_dir!(".")
+# architecture
+arch = CPU()
 
-# architecture and dimension
-arch = GPU()
-dim = 2
-
-# params/funcs
-ε = 2e-2
-α = 1/2
-μϱ = 1e0
-N² = 1e0/α
-Δt = 1e-5*μϱ/ε^2/α^2
+# params
+ε = 2e-1   # Ekman number
+α = 1/2    # aspect ratio
+μϱ = 1     # Prandtl times Burger number
+N² = 1/α     # background stratification (if you want `b` to be a perturbation from N²z)
+Δt = 1e-3  # time step
 params = Parameters(ε, α, μϱ, N², Δt)
+T = 0.1*μϱ/ε^2  # simulation time
 f₀ = 1.0
-β = 0.0
-f(x) = f₀ + β*x[2]
-H(x) = α*(1 - x[1]^2 - x[2]^2)
-ν(x) = 1
-κ(x) = 1e-2 + exp(-(x[3] + H(x))/(0.1*α))
-T = 5e-2*μϱ/ε^2/α^2
+β = 0.5
+f(x) = f₀ + β*x[2]  # Coriolis parameter
+H(x) = α*(1 - x[1]^2 - x[2]^2)  # bathymetry
+ν(x) = 1  # viscosity
+κ(x) = 1e-2 + exp(-(x[3] + H(x))/(0.1*α))  # diffusivity
+τˣ(x) = 0  # zonal wind stress
+τʸ(x) = 0  # meridional wind stress
+b₀(x) = 0  # surface buoyancy boundary condition
+force_build_inversion = false
+force_build_evolution = true
 
 # mesh
-h = 7e-3
-mesh = Mesh(@sprintf("../meshes/bowl%sD_%e_%e.msh", dim, h, α))
+h = 8e-2
+dim = 3
+mesh_name = @sprintf("bowl%dD_%e_%e", dim, h, α)
+mesh = Mesh(joinpath(@__DIR__, "../meshes/$mesh_name.msh"))
+
+# FE data
+spaces = Spaces(mesh, b₀)
+fe_data = FEData(mesh, spaces)
+@info "DOFs: $(fe_data.dofs.nu + fe_data.dofs.nv + fe_data.dofs.nw + fe_data.dofs.np)" 
 
 # build inversion matrices
-A_inversion_fname = @sprintf("../matrices/A_inversion_%sD_%e_%e_%e_%e_%e.jld2", dim, h, ε, α, f₀, β)
-if !isfile(A_inversion_fname) 
+A_inversion_fname = joinpath(@__DIR__, "../matrices/A_inversion_$mesh_name.jld2")
+if force_build_inversion
+    @warn "You set `force_build_inversion` to `true`, building matrices..."
+    A_inversion, B_inversion, b_inversion = build_inversion_matrices(fe_data, params, f, ν, τˣ, τʸ; A_inversion_ofile=A_inversion_fname)
+elseif !isfile(A_inversion_fname) 
     @warn "A_inversion file not found, generating..."
-    A_inversion, B_inversion = build_inversion_matrices(mesh, params, f, ν; A_inversion_ofile=A_inversion_fname)
+    A_inversion, B_inversion, b_inversion = build_inversion_matrices(fe_data, params, f, ν, τˣ, τʸ; A_inversion_ofile=A_inversion_fname)
 else
     file = jldopen(A_inversion_fname, "r")
     A_inversion = file["A_inversion"]
     close(file)
-    B_inversion = nuPGCM.build_B_inversion(mesh, params)
+    B_inversion = nuPGCM.build_B_inversion(fe_data, params)
+    b_inversion = nuPGCM.build_b_inversion(fe_data, params, τˣ, τʸ)
 end
 
 # re-order dofs
-A_inversion = A_inversion[mesh.dofs.p_inversion, mesh.dofs.p_inversion]
-B_inversion = B_inversion[mesh.dofs.p_inversion, :]
+A_inversion = A_inversion[fe_data.dofs.p_inversion, fe_data.dofs.p_inversion]
+B_inversion = B_inversion[fe_data.dofs.p_inversion, :]
+b_inversion = b_inversion[fe_data.dofs.p_inversion]
 
 # preconditioner
 if typeof(arch) == CPU
-    P_inversion = lu(A_inversion)
+    @time "lu(A_inversion)" P_inversion = lu(A_inversion)
 else
     P_inversion = Diagonal(on_architecture(arch, 1/h^dim*ones(size(A_inversion, 1))))
 end
@@ -59,9 +70,10 @@ end
 # move to arch
 A_inversion = on_architecture(arch, A_inversion)
 B_inversion = on_architecture(arch, B_inversion)
+b_inversion = on_architecture(arch, b_inversion)
 
 # setup inversion toolkit
-inversion_toolkit = InversionToolkit(A_inversion, P_inversion, B_inversion)
+inversion_toolkit = InversionToolkit(A_inversion, P_inversion, B_inversion, b_inversion; atol=1e-6, rtol=1e-6)
 
 # # quick inversion here:
 # model = inversion_model(arch, params, mesh, inversion_toolkit)
@@ -69,29 +81,16 @@ inversion_toolkit = InversionToolkit(A_inversion, P_inversion, B_inversion)
 # invert!(model)
 # save_state(model, "$out_dir/data/state.jld2")
 
-# build evolution matrices and test against saved matrices
-θ = Δt/2 * α^2 * ε^2 / μϱ 
-A_diff_fname = @sprintf("../matrices/A_diff_%sD_%e_%e_%e.jld2", dim, h, θ, α)
-A_adv_fname = @sprintf("../matrices/A_adv_%sD_%e_%e.jld2", dim, h, α)
-if !isfile(A_diff_fname) || !isfile(A_adv_fname)
-    @warn "A_diff or A_adv file not found, generating..."
-    A_adv, A_diff, B_diff, b_diff = build_evolution_matrices(mesh, params, κ; 
-                                        A_adv_ofile=A_adv_fname, A_diff_ofile=A_diff_fname)
-else
-    file = jldopen(A_adv_fname, "r")
-    A_adv = file["A_adv"]
-    close(file)
-    file = jldopen(A_diff_fname, "r")
-    A_diff = file["A_diff"]
-    close(file)
-    B_diff, b_diff = nuPGCM.build_B_diff_b_diff(mesh, params, κ)
-end
+# build evolution matrices (or load them if `force_build` is false and file exists)
+A_adv, A_diff, B_diff, b_diff = build_evolution_system(fe_data, params, κ; 
+                                    force_build=force_build_evolution,
+                                    filename=joinpath(@__DIR__, "../matrices/evolution_$mesh_name.jld2"))
 
 # re-order dofs
-A_adv  = A_adv[mesh.dofs.p_b, mesh.dofs.p_b]
-A_diff = A_diff[mesh.dofs.p_b, mesh.dofs.p_b]
-B_diff = B_diff[mesh.dofs.p_b, :]
-b_diff = b_diff[mesh.dofs.p_b]
+A_adv  =  A_adv[fe_data.dofs.p_b, fe_data.dofs.p_b]
+A_diff = A_diff[fe_data.dofs.p_b, fe_data.dofs.p_b]
+B_diff = B_diff[fe_data.dofs.p_b, :]
+b_diff = b_diff[fe_data.dofs.p_b]
 
 # preconditioners
 if typeof(arch) == CPU 
@@ -112,9 +111,16 @@ b_diff = on_architecture(arch, b_diff)
 evolution_toolkit = EvolutionToolkit(A_adv, P_adv, A_diff, P_diff, B_diff, b_diff)
 
 # put it all together in the `model` struct
-model = rest_state_model(arch, params, mesh, inversion_toolkit, evolution_toolkit)
+model = rest_state_model(arch, params, fe_data, inversion_toolkit, evolution_toolkit)
+
+# set initial buoyancy
+set_b!(model, x->b₀(x))
+invert!(model) # sync flow with initial condition 
+save_vtk(model, ofile=@sprintf("%s/data/state_%016d.vtu", out_dir, 0))
 
 # solve
-run!(model, T; t_plot=T÷5, t_save=T÷5)
+n_steps = Int(round(T / Δt))
+n_save = n_steps ÷ 100
+run!(model; n_steps, n_save)
 
 println("Done.")
