1fb31ce95d9fc0391b99a5a969b8691a5d46c49a
Henry Peterson <hgpeterson@caltech.edu>
2025-09-17 17:19:00 -0700

    Add mesh tests

--- test/matrix_assembly_tests.jl
+++ test/matrix_assembly_tests.jl
@@ -1,13 +1,28 @@
 using Test
 using nuPGCM.FiniteElements
 
-mesh = FiniteElements.Mesh(joinpath(@__DIR__, "../meshes/bowl2D_1.000000e-02_5.000000e-01.msh"))
+# mesh = FiniteElements.Mesh(joinpath(@__DIR__, "../meshes/bowl2D_1.000000e-02_5.000000e-01.msh"))
+h = 0.001
+nodes = [0.0 0.0 0.0
+         0.0 1.0 0.0
+         1.0 0.0 0.0
+         1.0 1.0 0.0]
+elements = [1 2 4
+            1 3 4]
+boundary_nodes = Dict("left" => [1, 2], 
+                      "right" => [3, 4], 
+                      "bottom" => [1, 3], 
+                      "top" => [2, 4])
+mesh = FiniteElements.Mesh(nodes, elements, boundary_nodes)
+
+
 jacs = Jacobians(mesh)
 quad = QuadratureRule(Triangle(); deg=2)
-space = Mini()
-M = FiniteElements.mass_matrix(mesh, jacs, quad, space; dirichlet_tags=["bot"])
-K = FiniteElements.stiffness_matrix(mesh, jacs, quad, space; dirichlet_tags=["bot"])
+space = P1()
+# space = Mini()
+M = FiniteElements.mass_matrix(mesh, jacs, quad, space; dirichlet=["bottom"])
+K = FiniteElements.stiffness_matrix(mesh, jacs, quad, space; dirichlet=["bottom"])
 rhs = M*ones(size(M, 1))
-rhs[FiniteElements.get_dirichlet_tags(mesh, ["bot"])] .= 1.0
-u = K\rhs
+rhs[FiniteElements.get_dirichlet_tags(mesh, ["bottom"])] .= 0.0
+u = K\rhs  #  FIXME: not getting -y^2/2 + y like I thought
 FiniteElements.save_vtu(mesh, joinpath(@__DIR__, "data/u.vtu"), Dict("u" => u))
